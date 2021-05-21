038f4911065e6d38f5aea2a603b8fad1d1ad7f38
Jiang Long <longjiang2005@gmail.com>
2021-05-20 22:13:04 -0700

    Add share sheet (via meta)

--- pages/dictionary.vue
+++ pages/dictionary.vue
@@ -27,7 +27,7 @@
           v-if="!entry"
           style="min-height: 10rem"
         >
-          {{ $t('For the love of {l2} words.', { l2: $t($l2.name) }) }}
+          {{ $t("For the love of {l2} words.", { l2: $t($l2.name) }) }}
         </h2>
       </div>
     </div>
@@ -48,19 +48,22 @@
         />
       </div>
     </div>
-    <DictionaryEntry
-      v-if="entry"
-      :entry="entry"
-      ref="dictionaryEntry"
-      :key="`dictionary-entry-${entry.id}`"
-    />
+    <article>
+      <SocialHead :title="title" :description="description" :image="image" />
+      <DictionaryEntry
+        v-if="entry"
+        :entry="entry"
+        ref="dictionaryEntry"
+        :key="`dictionary-entry-${entry.id}`"
+      />
+    </article>
   </div>
 </template>
 
 <script>
-import SearchCompare from '@/components/SearchCompare.vue'
-import Paginator from '@/components/Paginator'
-import DictionaryEntry from '@/components/DictionaryEntry'
+import SearchCompare from "@/components/SearchCompare.vue";
+import Paginator from "@/components/Paginator";
+import DictionaryEntry from "@/components/DictionaryEntry";
 
 export default {
   components: {
@@ -82,174 +85,195 @@ export default {
       entry: undefined,
       entryKey: 0,
       paginatorKey: 0,
-      title: '',
-      description: '',
+    };
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description
+        }
+      ]
     }
   },
   methods: {
     async updateWords() {
-      this.sW = []
-      this.savedTexts = []
+      this.sW = [];
+      this.savedTexts = [];
       if (this.$root.savedWords && this.$root.savedWords[this.$l2.code]) {
         for (let savedWord of this.$root.savedWords[this.$l2.code]) {
-          let word = await (await this.$dictionary).get(savedWord.id)
+          let word = await (await this.$dictionary).get(savedWord.id);
           if (word) {
-            this.sW.push(word)
+            this.sW.push(word);
           }
         }
       }
-      this.paginatorKey++
+      this.paginatorKey++;
     },
     saved() {
       return (
         this.entry &&
-        this.$store.getters['savedWords/has']({
+        this.$store.getters["savedWords/has"]({
           text: this.entry.bare.toLowerCase(),
           l2: this.$l2.code,
         })
-      )
+      );
     },
     async show(entry) {
       this.entry = entry
-      this.title = `${entry.bare} ${
-        entry.pronunciation ? '(' + entry.pronunciation + ')' : ''
-      } | ${this.$l2 ? this.$l2.name : ''} Zero to Hero Dictionary`
-      this.description = `"${entry.bare}" means ${entry.definitions.join(
-        '; '
-      )}:`
     },
     async route() {
       if (this.method && this.args) {
         if (this.method === this.$dictionaryName) {
-          if (this.args === 'random') {
-            this.random()
+          if (this.args === "random") {
+            this.random();
           } else {
-            let entry = await (await this.$dictionary).get(this.args)
-            this.show(entry)
+            let entry = await (await this.$dictionary).get(this.args);
+            this.show(entry);
           }
-        } else if (this.method === 'hsk') {
-          let entry = await (await this.$dictionary).getByHSKId(this.args)
-          this.show(entry)
+        } else if (this.method === "hsk") {
+          let entry = await (await this.$dictionary).getByHSKId(this.args);
+          this.show(entry);
         }
       }
     },
     async random() {
-      let randomId = (await (await this.$dictionary).random()).id
+      let randomId = (await (await this.$dictionary).random()).id;
       this.$router.push({
         path: `/${this.$l1.code}/${this.$l2.code}/dictionary/${this.$dictionaryName}/${randomId}`,
-      })
+      });
     },
 
     bindKeys() {
-      window.addEventListener('keydown', this.keydown)
+      window.addEventListener("keydown", this.keydown);
     },
     unbindKeys() {
-      window.removeEventListener('keydown', this.keydown)
+      window.removeEventListener("keydown", this.keydown);
     },
 
     keydown(e) {
       if (
-        !['INPUT', 'TEXTAREA'].includes(e.target.tagName.toUpperCase()) &&
+        !["INPUT", "TEXTAREA"].includes(e.target.tagName.toUpperCase()) &&
         !e.metaKey
       ) {
         // home
         if (e.keyCode == 36) {
-          document.getElementById('main').scrollIntoView({ behavior: 'smooth' })
+          document
+            .getElementById("main")
+            .scrollIntoView({ behavior: "smooth" });
           // this.$refs.searchCompare.focusOnSearch()
-          e.preventDefault()
-          return false
+          e.preventDefault();
+          return false;
         }
         // end
         if (e.keyCode == 35) {
           document
-            .getElementById('search-subs')
-            .scrollIntoView({ behavior: 'smooth' })
-          e.preventDefault()
-          return false
+            .getElementById("search-subs")
+            .scrollIntoView({ behavior: "smooth" });
+          e.preventDefault();
+          return false;
         }
         // n = 78
         if (e.keyCode == 78) {
-          this.$refs.dictionaryEntry.$refs.entryHeader.nextWord()
-          document.getElementById('main').scrollIntoView({ behavior: 'smooth' })
-          e.preventDefault()
-          return false
+          this.$refs.dictionaryEntry.$refs.entryHeader.nextWord();
+          document
+            .getElementById("main")
+            .scrollIntoView({ behavior: "smooth" });
+          e.preventDefault();
+          return false;
         }
         // p = 80
         if (e.keyCode == 80) {
-          this.$refs.dictionaryEntry.$refs.entryHeader.prevWord()
-          document.getElementById('main').scrollIntoView({ behavior: 'smooth' })
-          e.preventDefault()
-          return false
+          this.$refs.dictionaryEntry.$refs.entryHeader.prevWord();
+          document
+            .getElementById("main")
+            .scrollIntoView({ behavior: "smooth" });
+          e.preventDefault();
+          return false;
         }
         // escape = 27
-        if (e.code == 'KeyS') {
-          let hit = this.$refs.dictionaryEntry.$refs.searchSubs.currentHit
+        if (e.code == "KeyS") {
+          let hit = this.$refs.dictionaryEntry.$refs.searchSubs.currentHit;
           if (hit.saved) {
-            console.log('key s - removing hit', this.$refs.dictionaryEntry.$refs.searchSubs.terms, hit)
-            this.$refs.dictionaryEntry.$refs.searchSubs.removeSavedHit(hit)
+            console.log(
+              "key s - removing hit",
+              this.$refs.dictionaryEntry.$refs.searchSubs.terms,
+              hit
+            );
+            this.$refs.dictionaryEntry.$refs.searchSubs.removeSavedHit(hit);
           } else {
-            console.log('key s - saving hit', this.$refs.dictionaryEntry.$refs.searchSubs.terms, hit)
-            this.$refs.dictionaryEntry.$refs.searchSubs.saveHit(hit)
+            console.log(
+              "key s - saving hit",
+              this.$refs.dictionaryEntry.$refs.searchSubs.terms,
+              hit
+            );
+            this.$refs.dictionaryEntry.$refs.searchSubs.saveHit(hit);
           }
-          e.preventDefault()
-          return false
+          e.preventDefault();
+          return false;
         }
       }
     },
   },
-  metaInfo() {
-    return {
-      meta: [
-        // Facebook OpenGraph
-        { property: 'og:title', content: this.title },
-        { property: 'og:site_name', content: this.$l2.name + ' Zero to Hero' },
-        { property: 'og:type', content: 'website' },
-        {
-          property: 'og:image',
-          content:
-            this.searchSubsImage ||
-            this.webImage ||
-            this.$languages.logo(this.$l2.code),
-        },
-        {
-          property: 'og:description',
-          content:
-            this.description + ' ' + this.searchSubsExample ||
-            this.entry.example ||
-            '',
-        },
-      ],
-    }
+  computed: {
+    title() {
+      if (this.entry) {
+        return `${this.entry.bare} ${
+          this.entry.pronunciation ? "(" + this.entry.pronunciation + ")" : ""
+        } | ${this.$l2 ? this.$l2.name : ""} Zero to Hero Dictionary`;
+      }
+      return `Dictionary | ${this.$l2 ? this.$l2.name : ""} Zero to Hero`;
+    },
+    description() {
+      if (this.entry) {
+        return `"${this.entry.bare}" means ${this.entry.definitions.join(
+          "; "
+        )}`;
+      }
+      return `Look up  ${this.$l2 ? this.$l2.name : ""} words.`;
+    },
+    image() {
+      if (this.entry) {
+        if (this.$refs.dictionaryEntry && this.$refs.dictionaryEntry.webImage) {
+          return this.$refs.dictionaryEntry.webImage
+        }
+        return this.$languages.logo(this.$l2.code);
+      }
+      return "/img/icon-z2h.jpeg";
+    },
   },
   watch: {
     $route() {
-      if (this.$route.name === 'dictionary') {
-        this.route()
+      if (this.$route.name === "dictionary") {
+        this.route();
       }
     },
   },
   activated() {
-    this.bindKeys()
+    this.bindKeys();
   },
   deactivated() {
-    this.unbindKeys()
+    this.unbindKeys();
   },
   mounted() {
-    if (this.$route.name === 'dictionary') {
-      this.route()
-      this.updateWords()
+    if (this.$route.name === "dictionary") {
+      this.route();
+      this.updateWords();
       this.unsubscribe = this.$store.subscribe((mutation, state) => {
-        if (mutation.type.startsWith('savedWords')) {
-          this.updateWords()
+        if (mutation.type.startsWith("savedWords")) {
+          this.updateWords();
         }
-      })
+      });
     }
   },
   beforeDestroy() {
     // you may call unsubscribe to stop the subscription
-    this.unsubscribe()
+    this.unsubscribe();
   },
-}
+};
 </script>
 
 <style>
