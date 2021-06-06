8b2a355fbb10bd6a33bc1f960c7ef17965e0769f
Jiang Long <longjiang2005@gmail.com>
2021-06-06 13:34:38 -0700

    Speak component uses Helper.speak()

--- components/Speak.vue
+++ components/Speak.vue
@@ -5,26 +5,10 @@
 </template>
 <script>
 import commons from 'wikimedia-commons-file-path'
+import Helper from '@/lib/helper'
+
 export default {
-  data() {
-    return {
-      availableLocales: [],
-      preferredLocales: [],
-      bestLocale: 'en',
-      bestVoice: undefined
-    }
-  },
   props: ['text', 'mp3', 'wiktionary'],
-  mounted() {
-    if(this.availableLocales.length === 0) {
-      this.init()
-    }
-  },
-  updated() {
-    if(this.availableLocales.length === 0) {
-      this.init()
-    }
-  },
   computed: {
     $l1() {
       if (typeof this.$store.state.settings.l1 !== "undefined")
@@ -45,35 +29,14 @@ export default {
     },
   },
   methods: {
-    init() {
-      let voices = speechSynthesis.getVoices()
-      this.availableLocales = voices.map((voice) => voice.lang)
-      if (this.$l2.code === 'yue') this.preferredLocales.push('zh-HK')
-      if (this.$l2.locales)
-        this.preferredLocales = this.preferredLocales.concat(this.$l2.locales)
-      for (let locale of this.preferredLocales) {
-        if (this.availableLocales.includes(locale)) {
-          this.bestLocale = locale
-          this.bestVoice = voices.find(voice => voice.lang === locale)
-          break
-        }
-      }
-    },
     speak() {
-      if(this.availableLocales.length === 0) {
-        this.init()
-      }
       if (this.mp3) {
         let url = this.wiktionary ? commons(`File:${this.mp3}`) : this.mp3
         let audio = new Audio(url)
         audio.play()
       } else if (this.text) {
         if (this.$hasFeature('speech')) {
-          var utterance = new SpeechSynthesisUtterance(this.text)
-          utterance.lang = this.bestLocale
-          utterance.voice = this.bestVoice
-          utterance.rate = 0.7
-          speechSynthesis.speak(utterance)
+          Helper.speak(this.text, this.$l2, 0.75)
         } else {
           window.open(`https://forvo.com/search/${this.text}/${this.$l2.code}`)
         }
