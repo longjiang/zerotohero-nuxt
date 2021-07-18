6c871da519a5c356c07950a55167300cd660fe37
Jiang Long <longjiang2005@gmail.com>
2021-07-18 15:04:40 -0700

    Improve ASS parsing.

--- components/YouTubeVideoCard.vue
+++ components/YouTubeVideoCard.vue
@@ -437,7 +437,7 @@ export default {
             let events = data.find((section) => section.section === "Events");
             if (events) {
               subs_l2 = events.body
-                .filter((item) => item.key === "Dialogue" && !item.value.Style.includes('CN'))
+                .filter((item) => item.key === "Dialogue" && !item.value.Style.includes('CN') && !item.value.Style.includes('STAFF'))
                 .map((cue) => {
                   return {
                     starttime: this.parseTime(cue.value.Start),
