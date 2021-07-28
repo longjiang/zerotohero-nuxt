6c8922ed0edf94e3425c6ad5f8cc70adba61e4fb
Jiang Long <longjiang2005@gmail.com>
2021-07-28 10:03:09 -0700

    More translation strings.

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -4,7 +4,9 @@ import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 
 export const i18n = new VueI18n({
+  locale: 'en',
   fallbackLocale: 'en',
+  formatFallbackMessages: true,
   messages: {
   }
 })
