1bf6024e3e93406ab7cf62fd9252e065ff779e40
Jiang Long <longjiang2005@gmail.com>
2021-08-02 11:28:53 -0700

    New channel layout.

--- pages/live-tv.vue
+++ pages/live-tv.vue
@@ -6,19 +6,38 @@
 </router>
 <template>
   <div class="container main mt-5 mb-5">
-    <LiveVideo v-if="currentChannel" :url="currentChannel.url" :key="`live-video-${currentChannel.tvgID}`" ref="liveVideo"/>
-    <div class="pt-2 pb-2" v-if="channels">
-      <b-button
-        variant="gray"
-        size="sm"
-        :class="{'channel-button': true, 'channel-button-current': currentChannel === channel}"
-        v-for="channel in channels"
-        :key="`channel-button-${channel.tvgID}`"
-        @click="setChannel(channel)"
-      >
-        <img v-if="channel.logo" :src="channel.logo" :alt="channel.name" />
-        <span v-else>{{ channel.name }}</span>
-      </b-button>
+    <div class="row">
+      <div class="col-lg-8 live-video-column">
+        <div class="live-tv-wrapper">
+          <LiveVideo
+            v-if="currentChannel"
+            :url="currentChannel.url"
+            :key="`live-video-${currentChannel.tvgID}`"
+            ref="liveVideo"
+          />
+          <div v-if="currentChannel" class="pt-2 pb-2">
+            <b>Channel:</b> {{ currentChannel.name }}
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="pt-2 pb-2" v-if="channels">
+          <b-button
+            variant="gray"
+            size="sm"
+            :class="{
+              'channel-button': true,
+              'channel-button-current': currentChannel === channel,
+            }"
+            v-for="channel in channels"
+            :key="`channel-button-${channel.tvgID}`"
+            @click="setChannel(channel)"
+          >
+            <img v-if="channel.logo" :src="channel.logo" :alt="channel.name" />
+            <span v-else>{{ channel.name }}</span>
+          </b-button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,7 +52,7 @@ export default {
   data() {
     return {
       channels: undefined,
-      currentChannel: undefined
+      currentChannel: undefined,
     };
   },
   computed: {
@@ -57,23 +76,37 @@ export default {
     if (res && res.data) {
       let channels = Papa.parse(res.data, { header: true }).data;
       channels = Helper.uniqueByValue(channels, "tvgID");
-      channels = channels.sort((a, b) => a.name.localeCompare(b.name, this.$l2.code));
+      channels = channels.sort((a, b) =>
+        a.name.localeCompare(b.name, this.$l2.code)
+      );
       channels = channels.sort((a, b) =>
         a.logo === b.logo ? 0 : a.logo ? -1 : 1
       );
       this.channels = channels;
-      if (channels[0]) this.currentChannel = channels[0]
+      if (channels[0]) this.currentChannel = channels[0];
     }
   },
   methods: {
     setChannel(channel) {
-      this.currentChannel = channel
-    }
-  }
+      this.currentChannel = channel;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.live-video-column {
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  background: white;
+  .live-tv-wrapper {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+    background: white;
+  }
+}
 .channel-button {
   &.channel-button-current {
     background-color: red;
