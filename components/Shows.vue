8ae19f5d850d2598570ac55b28e0034afbb59857
Jiang Long <longjiang2005@gmail.com>
2021-08-03 12:52:38 -0700

    Add watch in random button.

--- components/Shows.vue
+++ components/Shows.vue
@@ -2,31 +2,33 @@
   <div class="main container" id="main">
     <SocialHead
       v-if="shows && shows[0]"
-      :title="`Learn ${$l2.name} with ${routeType === 'tv-shows' ? 'TV Shows' : 'Talks' } | ${$l2.name} Zero to Hero`"
-      :description="`Learn ${$l2.name} with ${routeType === 'tv-shows' ? 'TV Shows' : 'Talks' }.`"
+      :title="`Learn ${$l2.name} with ${
+        routeType === 'tv-shows' ? 'TV Shows' : 'Talks'
+      } | ${$l2.name} Zero to Hero`"
+      :description="`Learn ${$l2.name} with ${
+        routeType === 'tv-shows' ? 'TV Shows' : 'Talks'
+      }.`"
       :image="`https://img.youtube.com/vi/${shows[0].youtube_id}/hqdefault.jpg`"
     />
     <div class="row">
       <div class="col-sm-12">
         <h3 class="text-center mt-5">
-          Study {{ $l2.name }} with {{ routeType === 'tv-shows' ? 'TV Shows' : 'Talks' }}
+          Study {{ $l2.name }} with
+          {{ routeType === "tv-shows" ? "TV Shows" : "Talks" }}
         </h3>
-        <p class="text-center mb-5" v-if="shows && shows.length">({{ shows.length }} show{{ shows.length > 1 ? 's' : '' }})</p>
-        <SimpleSearch
-          class="mb-5"
-          placeholder="Search"
-          ref="searchLibrary"
-          :random="`/${$l1.code}/${$l2.code}/youtube/view/${randomEpisodeYouTubeId}`"
-          :action="
-            (url) => {
-              this.$router.push({
-                path: `/${$l1.code}/${
-                  $l2.code
-                }/youtube/browse/all/all/0/${encodeURIComponent(url)}`,
-              });
-            }
-          "
-        />
+        <p class="text-center mb-4" v-if="shows && shows.length">
+          ({{ shows.length }} show{{ shows.length > 1 ? "s" : "" }})
+        </p>
+        <div class="text-center mb-5">
+          <NuxtLink
+            :to="`/${$l1.code}/${$l2.code}/youtube/view/${randomEpisodeYouTubeId}`"
+            class="btn btn-success d-inline-block text-large pl-4 pr-4"
+            style="font-size: 1.3em"
+          >
+            <i class="fa fa-random"></i>
+            Watch in random
+          </NuxtLink>
+        </div>
         <div class="mb-5">
           <div
             :class="{
@@ -38,9 +40,15 @@
             <div class="heartbeat-loader"></div>
           </div>
           <div class="text-center" v-if="shows && shows.length === 0">
-            Sorry, we could not find any {{ routeType === 'tv-shows' ? 'TV shows' : 'talks' }} in {{ $l2.name }} 😭.
+            Sorry, we could not find any
+            {{ routeType === "tv-shows" ? "TV shows" : "talks" }} in
+            {{ $l2.name }} 😭.
           </div>
-          <ShowList v-if="shows && shows.length > 0" :shows="shows" :type="type" />
+          <ShowList
+            v-if="shows && shows.length > 0"
+            :shows="shows"
+            :type="type"
+          />
         </div>
       </div>
     </div>
@@ -48,32 +56,36 @@
 </template>
 
 <script>
-import Config from '@/lib/config'
+import Config from "@/lib/config";
 import Helper from "@/lib/helper";
 import YouTube from "@/lib/youtube";
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   props: {
-    routeType: String
+    routeType: String,
   },
   data() {
     return {
-      type: this.routeType === 'tv-shows' ? 'tvShows' : 'talks',
+      type: this.routeType === "tv-shows" ? "tvShows" : "talks",
       shows: undefined,
       randomEpisodeYouTubeId: undefined,
     };
   },
   async fetch() {
-    let shows = this.$store.state.shows[this.type][this.$l2.code]
+    let shows = this.$store.state.shows[this.type][this.$l2.code];
     if (shows) {
-      this.shows = shows
+      this.shows = shows;
     } else {
       let response = await axios.get(
-        `${Config.wiki}items/${this.routeType.replace('-', '_')}?filter[l2][eq]=${this.$l2.id
-        }&limit=500&timestamp=${this.$adminMode ? Date.now() : 0}`
+        `${Config.wiki}items/${this.routeType.replace(
+          "-",
+          "_"
+        )}?filter[l2][eq]=${this.$l2.id}&limit=500&timestamp=${
+          this.$adminMode ? Date.now() : 0
+        }`
       );
-      if (response && response.data) this.shows = response.data.data
+      if (response && response.data) this.shows = response.data.data;
     }
   },
   async mounted() {
@@ -84,7 +96,7 @@ export default {
     });
     this.randomEpisodeYouTubeId = await YouTube.getRandomEpisodeYouTubeId(
       this.$l2.id,
-      this.routeType === 'tv-shows' ? 'tv_show' : 'talk',
+      this.routeType === "tv-shows" ? "tv_show" : "talk"
     );
   },
   beforeDestroy() {
