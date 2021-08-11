33cd5210d651eace232bfb565868440d4238a149
Jiang Long <longjiang2005@gmail.com>
2021-08-11 13:20:35 -0700

    Use container query for TV shows.

--- components/ShowList.vue
+++ components/ShowList.vue
@@ -1,46 +1,83 @@
 <template>
-  <div class="tv-shows row">
-    <div
-      class="col-xs-12 col-sm-6 col-md-4 col-lg-3"
-      v-for="show of shows"
-      :key="`tv-show-wrapper-${show.id}`"
-      style="padding-bottom: 2rem"
-    >
-      <div class="tv-show media rounded shadow">
-        <router-link
-          class="youtube-thumbnail-wrapper aspect-wrapper d-block"
-          :to="path(show)"
-        >
-          <img
-            :src="`//img.youtube.com/vi/${show.youtube_id}/hqdefault.jpg`"
-            class="youtube-thumbnail aspect"
-            v-lazy-load
-          />
-        </router-link>
-        <div class="media-body">
-          <router-link :to="path(show)" class="link-unstyled">
-            <h6>
-              {{ show.title }}
-            </h6>
-            <b-button
-              v-if="$adminMode"
-              class="btn btn-small bg-danger text-white mt-2 ml-0"
-              @click.stop.prevent="remove(show)"
-            >
-              <i class="fa fa-trash"></i>
-            </b-button>
+  <container-query :query="query" v-model="params">
+    <div class="tv-shows row">
+      <div
+        :class="{
+          'col-12': params.xs,
+          'col-6': params.sm,
+          'col-4': params.md,
+          'col-3': params.lg,
+        }"
+        v-for="show of shows"
+        :key="`tv-show-wrapper-${show.id}`"
+        style="padding-bottom: 2rem"
+      >
+        <div class="tv-show media rounded shadow">
+          <router-link
+            class="youtube-thumbnail-wrapper aspect-wrapper d-block"
+            :to="path(show)"
+          >
+            <img
+              :src="`//img.youtube.com/vi/${show.youtube_id}/hqdefault.jpg`"
+              class="youtube-thumbnail aspect"
+              v-lazy-load
+            />
           </router-link>
+          <div class="media-body">
+            <router-link :to="path(show)" class="link-unstyled">
+              <h6>
+                {{ show.title }}
+              </h6>
+              <b-button
+                v-if="$adminMode"
+                class="btn btn-small bg-danger text-white mt-2 ml-0"
+                @click.stop.prevent="remove(show)"
+              >
+                <i class="fa fa-trash"></i>
+              </b-button>
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </container-query>
 </template>
 
 <script>
+import { ContainerQuery } from "vue-container-query";
 export default {
+  components: {
+    ContainerQuery,
+  },
   props: {
     shows: Array,
     type: String, // 'tvShows' or 'talks'
+  },
+  data() {
+    return {
+      params: {},
+      query: {
+        xs: {
+          minWidth: 0,
+          maxWidth: 540,
+        },
+        sm: {
+          minWidth: 540,
+          maxWidth: 720,
+        },
+        md: {
+          minWidth: 720,
+          maxWidth: 960,
+        },
+        lg: {
+          minWidth: 960,
+          maxWidth: 1140,
+        },
+        xl: {
+          minWidth: 1140,
+        },
+      },
+    };
   },
   methods: {
     async remove(show) {
