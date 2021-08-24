1c2a543edf5861e88fe613375651823a9548cb86
Jiang Long <longjiang2005@gmail.com>
2021-08-23 22:35:37 -0700

    Add language map component.

---
+++ components/LanguageMap.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="map-wrap" style="height: 100vh">
+    <client-only>
+      <l-map :zoom="13" :center="[55.9464418, 8.1277591]">
+        <l-tile-layer
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        ></l-tile-layer>
+        <l-marker :lat-lng="[55.9464418, 8.1277591]"></l-marker>
+      </l-map>
+    </client-only>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
+
+<style>
+</style>
---
+++ pages/language-map.vue
@@ -0,0 +1,25 @@
+<router>
+  {
+    path: '/language-map',
+    props: true
+  }
+</router>
+<template>
+  <div class="main">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <LanguageMap />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import LanguageMap from "@/components/LanguageMap";
+export default {};
+</script>
+
+<style>
+</style>
