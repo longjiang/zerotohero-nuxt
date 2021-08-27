c1b661586ca942aa5d06743b5a54316f2959ffcb
Jiang Long <longjiang2005@gmail.com>
2021-08-26 22:39:45 -0700

    Move languages.csv and countries.csv to local.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
 export default {
+  env: {
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Zero to Hero Languages',
@@ -114,26 +117,22 @@ export default {
       } else {
         config.devtool = false
       }
-
-
-      // if (isClient) {
-      //   config.node = {
-      //     fs: 'empty',
-      //     child_process: 'empty',
-      //     canva: 'empty',
-      //   }
-      // }
     }
   },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: 'http://localhost:3000', // Used as fallback if no runtime config is provided
+  },
 
-  generate: {
-    routes: [
-      // '/en/zh/dictionary/hsk-cedict/%E7%95%99%E5%B0%BE%E5%B7%B4,li%C3%BA_w%C4%9Bi_ba,0',
-      // '/en/zh/dictionary/hsk-cedict/%E5%8D%81%E6%88%92,sh%C3%AD_ji%C3%A8,0',
-      // '/en/zh/dictionary/hsk-cedict/%E9%9B%95%E5%88%BB,di%C4%81o_k%C3%A8,0'
-    ]
-  }
+  publicRuntimeConfig: {
+    axios: {
+      browserBaseURL: process.env.BROWSER_BASE_URL
+    }
+  },
+
+  privateRuntimeConfig: {
+    axios: {
+      baseURL: process.env.BASE_URL
+    }
+  },
 }
