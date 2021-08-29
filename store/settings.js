043d0b09f9bcbf73a7062094eb687ad7223fb9aa
Jiang Long <longjiang2005@gmail.com>
2021-08-29 00:47:29 -0700

    Default to 'no romanization' for vietnamese.

--- store/settings.js
+++ store/settings.js
@@ -17,7 +17,7 @@ export const state = () => {
       showByeonggi: true,
       disableAnnotation: false
     },
-    romanizationOffByDefault: ['ru', 'ko', 'bo', 'dz', 'ru', 'th', 'my', 'hy', 'uk', 'tt']
+    romanizationOffByDefault: ['ru', 'ko', 'bo', 'dz', 'ru', 'th', 'my', 'hy', 'uk', 'tt', 'vi']
   }
 }
 
