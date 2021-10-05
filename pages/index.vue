75d56a4e43867931996e1de31819e107bacf1e43
Jiang Long <longjiang2005@gmail.com>
2021-10-05 14:00:00 -0700

    Do not lazy-load images on the home page above the fold.

--- pages/index.vue
+++ pages/index.vue
@@ -138,24 +138,28 @@
                   <img
                     src="/img/logo-youtube.png"
                     style="height: 1.2rem; padding: 0 0.5rem"
+                    data-not-lazy
                   />
                 </a>
                 <a href="https://www.instagram.com/chinesezerotohero/">
                   <img
                     src="/img/logo-instagram.png"
                     style="height: 1.4rem; padding: 0 0.5rem"
+                    data-not-lazy
                   />
                 </a>
                 <a href="http://chinesezerotohero.teachable.com/">
                   <img
                     src="/img/logo-teachable.png"
                     style="height: 1.4rem; padding: 0 0.5rem"
+                    data-not-lazy
                   />
                 </a>
                 <a href="http://t.me/zerotohero_zh">
                   <img
                     src="/img/logo-telegram.png"
                     style="height: 1.4rem; padding: 0 0.5rem"
+                    data-not-lazy
                   />
                 </a>
               </div>
@@ -215,25 +219,45 @@
                 style="line-height: 2rem"
               >
                 <a href="https://m.cctalk.com/inst/stevmab3">
-                  <img src="/img/logo-cctalk.png" style="height: 1.2rem" />
+                  <img
+                    src="/img/logo-cctalk.png"
+                    style="height: 1.2rem"
+                    data-not-lazy
+                  />
                 </a>
                 &nbsp;&nbsp;
                 <a href="https://space.bilibili.com/253569339">
-                  <img src="/img/logo-bilibili.png" style="height: 1.2rem" />
+                  <img
+                    src="/img/logo-bilibili.png"
+                    style="height: 1.2rem"
+                    data-not-lazy
+                  />
                 </a>
                 &nbsp;&nbsp;
                 <a href="https://v.douyin.com/eNJCcD8/">
-                  <img src="/img/logo-douyin.png" style="height: 1.4rem" />
+                  <img
+                    src="/img/logo-douyin.png"
+                    style="height: 1.4rem"
+                    data-not-lazy
+                  />
                 </a>
                 &nbsp;&nbsp;
                 <a
                   href="https://www.xiaohongshu.com/user/profile/5fad77c0000000000100696e"
                 >
-                  <img src="/img/logo-xiaohongshu.png" style="height: 1.4rem" />
+                  <img
+                    src="/img/logo-xiaohongshu.png"
+                    style="height: 1.4rem"
+                    data-not-lazy
+                  />
                 </a>
                 &nbsp;&nbsp;
                 <a href="/zh/en/contact-us">
-                  <img src="/img/logo-wechat.png" style="height: 1.4rem" />
+                  <img
+                    src="/img/logo-wechat.png"
+                    style="height: 1.4rem"
+                    data-not-lazy
+                  />
                 </a>
               </div>
             </div>
@@ -249,6 +273,7 @@
                     src="/img/thumbnail-language-map.jpg"
                     alt="World Language Map"
                     class="rounded shadow img-fluid"
+                    data-not-lazy
                   />
                 </div>
               </router-link>
@@ -263,6 +288,7 @@
                     src="/img/thumbnail-compare-languages.jpg"
                     alt="Compare phrases across languages on a map"
                     class="rounded shadow img-fluid"
+                    data-not-lazy
                   />
                 </div>
               </router-link>
@@ -279,6 +305,7 @@
                     src="/img/thumbnail-language-icons.jpg"
                     alt="Faces of Languages"
                     class="rounded shadow img-fluid"
+                    data-not-lazy
                   />
                 </div>
               </router-link>
@@ -293,6 +320,7 @@
                     src="/img/thumbnail-translators.jpg"
                     alt="Faces of Languages"
                     class="rounded shadow img-fluid"
+                    data-not-lazy
                   />
                 </div>
               </router-link>
