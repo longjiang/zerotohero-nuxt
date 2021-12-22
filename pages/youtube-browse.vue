2f61533b2022f0caa40040d6eb526ec6c8b6d664
Jiang Long <longjiang2005@gmail.com>
2021-12-21 23:48:23 -0800

    Do not show discover player on 'new videos' page if we don't have videos.

--- pages/youtube-browse.vue
+++ pages/youtube-browse.vue
@@ -28,7 +28,7 @@
           <div
             class="col-sm-12"
           >
-            <LazyDiscoverPlayer class="mt-5" routeType="talks" :shows="[]" />
+            <LazyDiscoverPlayer v-if="videos && videos[0]" class="mt-5" routeType="talks" :shows="[]" />
           </div>
           <!-- <client-only>
             <Nav
