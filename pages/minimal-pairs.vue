a93a8b367a2095f9112e10cf110030e67b8174e2
Jiang Long <longjiang2005@gmail.com>
2022-04-26 13:30:06 -0700

    Add Chinese and Vietnamese presets to minimal pairs

--- pages/minimal-pairs.vue
+++ pages/minimal-pairs.vue
@@ -11,27 +11,42 @@
         <div class="col-sm-12">
           <h3 class="mb-5 text-center">{{ $l2.name }} Minimal Pairs</h3>
           <p class="text-center mb-3">Enter two distinct phonetic segments:</p>
-          <b-input-group>
-            <b-form-input
-              v-model.lazy="a"
-              type="text"
-              placeholder="a"
-            ></b-form-input>
-            <b-form-input
-              v-model.lazy="b"
-              type="text"
-              placeholder="b"
-            ></b-form-input>
-            <b-input-group-append>
-              <b-button
-                variant="primary"
-                @click="findMinimalPairs"
-                :disabled="!(a && b)"
-              >
-                Find Minimal Pairs
-              </b-button>
-            </b-input-group-append>
-          </b-input-group>
+          <div style="max-width: 20rem; margin: 0 auto">
+            <b-input-group>
+              <b-form-input
+                v-model.lazy="a"
+                type="text"
+                placeholder="a"
+                style="max-width: 5rem"
+              ></b-form-input>
+              <b-form-input
+                v-model.lazy="b"
+                type="text"
+                placeholder="b"
+                style="max-width: 5rem"
+              ></b-form-input>
+              <b-input-group-append>
+                <b-button
+                  variant="primary"
+                  @click="findMinimalPairs"
+                  :disabled="!(a && b)"
+                >
+                  Find Minimal Pairs
+                </b-button>
+              </b-input-group-append>
+            </b-input-group>
+          </div>
+          <div
+            v-if="defaults[$l2.code] && defaults[$l2.code].length > 1"
+            class="mt-4 text-center"
+          >
+            <p class="mb-3">Or use a preset:</p>
+            <b-form-select
+              v-model="presetSelect"
+              :options="presetOptions"
+              style="max-width: 15rem"
+            ></b-form-select>
+          </div>
         </div>
       </div>
       <div class="row">
@@ -91,27 +106,51 @@ export default {
       a: undefined,
       b: undefined,
       defaults: {
-        ko: ["사", "싸"],
-        zh: ["āng", "àng"],
-        vi: ["˧˧", "˨˩"],
-        ja: ["また", "まだ"],
-        ru: ["ш", "щ"],
-        en: ["ɪt", "iːt"],
-        fr: ['ɑ̃', 'œ̃'],
-        es: ['ɾo', 'ro'],
-        de: ['ʏ', 'yː']
+        ko: [["사", "싸"]],
+        zh: [
+          ["uāng", "uáng"],
+          ["uāng", "uǎng"],
+          ["uāng", "uàng"],
+          ["ā", "a"],
+          ["uáng", "uǎng"],
+          ["uáng", "uàng"],
+          ["á", "a"],
+          ["uǎng", "uàng"],
+          ["ǎ", "a"],
+          ["à", "a"],
+        ],
+        vi: [
+          ["˧˧", "˨˩"],
+          ["˧˧", "˨˩˦"],
+          ["˧˧", "˦˥"],
+          ["˧˧", "˨˩˨"],
+          ["˨˩", "˨˩˦"],
+          ["˨˩", "˦˥"],
+          ["˨˩", "˨˩˨"],
+          ["˨˩˦", "˦˥"],
+          ["˨˩˦", "˨˩˨"],
+          ["˦˥", "˨˩˨"],
+        ],
+        ja: [["また", "まだ"]],
+        ru: [["ш", "щ"]],
+        en: [["ɪt", "iːt"]],
+        fr: [["ɑ̃", "œ̃"]],
+        es: [["ɾo", "ro"]],
+        de: [["ʏ", "yː"]],
       },
       crunching: false,
       minimalPairs: undefined,
       numRowsVisible: 10,
       perPage: 10,
+      presetSelect: 0,
     };
   },
   mounted() {
     if (this.defaults[this.$l2.code]) {
-      this.a = this.defaults[this.$l2.code][0];
-      this.b = this.defaults[this.$l2.code][1];
+      this.a = this.defaults[this.$l2.code][0][0];
+      this.b = this.defaults[this.$l2.code][0][1];
     }
+    if (this.a && this.b) this.findMinimalPairs();
   },
   computed: {
     $l1() {
@@ -123,6 +162,28 @@ export default {
     filteredRows() {
       return this.minimalPairs.slice(0, this.numRowsVisible);
     },
+    presetOptions() {
+      if (this.defaults[this.$l2.code]) {
+        let options = [];
+        options = options.concat(
+          this.defaults[this.$l2.code].map((pair, index) => {
+            return {
+              value: index,
+              text: `${index + 1}. ${pair[0]} vs ${pair[1]}`,
+            };
+          })
+        );
+        return options;
+      }
+    },
+  },
+  watch: {
+    presetSelect() {
+      let selected = this.defaults[this.$l2.code][this.presetSelect];
+      this.a = selected[0];
+      this.b = selected[1];
+      this.findMinimalPairs();
+    },
   },
   methods: {
     infiniteScroll(isVisible) {
@@ -133,7 +194,10 @@ export default {
     async findMinimalPairs() {
       let dictionaryName = this.$store.state.settings.dictionaryName;
       let property = "pronunciation";
-      if (["kengdic", "openrussian"].includes(dictionaryName) || this.$l2.code === 'it')
+      if (
+        ["kengdic", "openrussian"].includes(dictionaryName) ||
+        this.$l2.code === "it"
+      )
         property = "head";
       if (dictionaryName === "edict") property = "kana";
       this.crunching = true;
@@ -158,7 +222,6 @@ export default {
       let bs = pronunciations.filter(
         (p) => p.chosenPornunciation.split(this.b).length === 2
       );
-      console.log(as, bs)
       let minimalPairs = [];
       for (let a of as) {
         let aSplit = a.chosenPornunciation.split(this.a);
