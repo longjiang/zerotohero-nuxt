eb7d2be18178894b0895920f4a46b40b6bc990d5
Jiang Long <longjiang2005@gmail.com>
2022-05-02 16:34:14 -0700

    subsearch menu should auto scroll to current hit.

--- components/SearchSubsComp.vue
+++ components/SearchSubsComp.vue
@@ -1,135 +1,72 @@
 <template>
-  <div
-    :class="{
-      'search-subs pb-3': true,
-      'search-subs-light': skin === 'dark',
-      'search-subs-dark': skin === 'dark',
-      fullscreen,
-    }"
-  >
-    <div
-      class="text-center pt-2 pb-2"
-      :style="fullscreenToggle && !$adminMode ? 'padding-left: 2.5rem' : ''"
-    >
+  <div :class="{
+    'search-subs pb-3': true,
+    'search-subs-light': skin === 'dark',
+    'search-subs-dark': skin === 'dark',
+    fullscreen,
+  }">
+    <div class="text-center pt-2 pb-2" :style="fullscreenToggle && !$adminMode ? 'padding-left: 2.5rem' : ''">
       <span v-if="hits.length > 0">
         <div :class="{ 'float-left ml-1': true, 'd-none': !$adminMode }">
-          <b-button
-            :variant="skin === 'dark' ? 'ghost-dark-no-bg' : 'gray'"
-            size="sm"
-            @click="remove"
-          >
+          <b-button :variant="skin === 'dark' ? 'ghost-dark-no-bg' : 'gray'" size="sm" @click="remove">
             <i class="fas fa-trash"></i>
           </b-button>
         </div>
-        <b-button
-          size="sm"
-          :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'"
-          :disabled="hitIndex === 0"
-          @click="goToPrevHit"
-          :class="{ 'ml-1 mr-1': true, disabled: hitIndex === 0 }"
-        >
+        <b-button size="sm" :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'" :disabled="hitIndex === 0"
+          @click="goToPrevHit" :class="{ 'ml-1 mr-1': true, disabled: hitIndex === 0 }">
           <i class="fas fa-step-backward" />
         </b-button>
-        <b-button
-          :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'"
-          size="sm"
-          v-if="!showFilter"
-          @click="showFilter = true"
-        >
+        <b-button :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'" size="sm" v-if="!showFilter"
+          @click="showFilter = true">
           <i class="fas fa-filter" />
         </b-button>
-        <b-form-input
-          v-if="!checking && (hits.length > 0 || regex) && showFilter"
-          type="text"
-          class="d-inline-block mr-1 ml-1"
-          size="sm"
-          v-model="regex"
-          placeholder="Filter..."
-          :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'"
-          :lazy="true"
-          :style="`width: 6em`"
-          @blur="showFilter = false"
-        />
+        <b-form-input v-if="!checking && (hits.length > 0 || regex) && showFilter" type="text"
+          class="d-inline-block mr-1 ml-1" size="sm" v-model="regex" placeholder="Filter..."
+          :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'" :lazy="true" :style="`width: 6em`"
+          @blur="showFilter = false" />
         <span class="search-subs-hit-index ml-2 mr-2 d-inline-block">
           {{ hitIndex + 1 }} of {{ hits.length }}
         </span>
-        <b-button
-          size="sm"
-          :variant="skin === 'dark' ? 'ghost-dark-no-bg' : 'gray'"
-          class="playlist-toggle ml-1 mr-1"
-          @click="showPlaylistModal"
-        >
+        <b-button size="sm" :variant="skin === 'dark' ? 'ghost-dark-no-bg' : 'gray'" class="playlist-toggle ml-1 mr-1"
+          @click="showPlaylistModal">
           <i class="fa fa-stream" />
         </b-button>
-        <router-link
-          v-if="currentHit"
-          :to="`/${$l1.code}/${$l2.code}/youtube/view/${
-            currentHit.video.youtube_id
-          }/?t=${currentHit.video.subs_l2[currentHit.lineIndex].starttime}`"
-          :class="`btn btn-${
-            skin === 'light' ? 'gray' : 'ghost-dark-no-bg'
-          } btn-sm mr-1 ml-1`"
-        >
+        <router-link v-if="currentHit" :to="`/${$l1.code}/${$l2.code}/youtube/view/${currentHit.video.youtube_id
+        }/?t=${currentHit.video.subs_l2[currentHit.lineIndex].starttime}`" :class="`btn btn-${skin === 'light' ? 'gray' : 'ghost-dark-no-bg'
+  } btn-sm mr-1 ml-1`">
           <i class="fa fa-window-restore" />
         </router-link>
-        <b-button
-          size="sm"
-          :disabled="hitIndex >= hits.length - 1"
-          :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'"
-          @click="goToNextHit"
-          :class="{
+        <b-button size="sm" :disabled="hitIndex >= hits.length - 1"
+          :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'" @click="goToNextHit" :class="{
             'ml-1 mr-1': true,
             disabled: hitIndex >= hits.length - 1,
-          }"
-        >
+          }">
           <i class="fas fa-step-forward" />
         </b-button>
         <div class="float-right mr-1">
-          <SmallStar
-            :item="currentHit"
-            :saved="(hit) => hit.saved"
-            :save="saveHit"
-            :remove="removeSavedHit"
-            class="ml-0 mr-0"
-            style="position: relative; bottom: -0.07em;"
-          />
-          <span
-            class="ml-1 mr-0"
-            style="background: none; position: relative; bottom: -0.07em; opacity: 0.7"
-            v-if="groupsRight['zthSaved'].length > 0"
-          >
+          <SmallStar :item="currentHit" :saved="(hit) => hit.saved" :save="saveHit" :remove="removeSavedHit"
+            class="ml-0 mr-0" style="position: relative; bottom: -0.07em;" />
+          <span class="ml-1 mr-0" style="background: none; position: relative; bottom: -0.07em; opacity: 0.7"
+            v-if="groupsRight['zthSaved'].length > 0">
             {{ groupsRight["zthSaved"].length }}
           </span>
-          <b-button
-            :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'"
-            class="search-subs-fullscreen"
-            size="sm"
-            @click="toggleFullscreen"
-            v-if="
+          <b-button :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'" class="search-subs-fullscreen" size="sm"
+            @click="toggleFullscreen" v-if="
               !checking &&
               !fullscreen &&
               fullscreenToggle &&
               (hits.length > 0 || regex)
-            "
-          >
+            ">
             <i class="fas fa-expand"></i>
           </b-button>
-          <b-button
-            :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'"
-            size="sm"
-            class="btn search-subs-close"
-            @click="toggleFullscreen"
-            v-if="!checking && fullscreen && fullscreenToggle"
-          >
+          <b-button :variant="skin === 'light' ? 'gray' : 'ghost-dark-no-bg'" size="sm" class="btn search-subs-close"
+            @click="toggleFullscreen" v-if="!checking && fullscreen && fullscreenToggle">
             <i class="fas fa-times" />
           </b-button>
         </div>
       </span>
     </div>
-    <div
-      :class="{ 'loader text-center pb-5 pt-3': true, 'd-none': !checking }"
-      style="flex: 1"
-    >
+    <div :class="{ 'loader text-center pb-5 pt-3': true, 'd-none': !checking }" style="flex: 1">
       <Loader :sticky="true" message="Searching through video captions..." />
     </div>
     <div class="text-center p-3" v-if="!checking && hits.length === 0">
@@ -139,131 +76,71 @@
         in
         <router-link :to="{ name: 'settings' }">Settings</router-link>
       </p>
-      <b-button
-        v-if="$adminMode"
-        size="sm"
-        variant="primary"
-        @click="checkHits"
-      >
+      <b-button v-if="$adminMode" size="sm" variant="primary" @click="checkHits">
         <i class="fa fa-sync-alt"></i>
         Refresh
       </b-button>
     </div>
-    <LazyYouTubeWithTranscript
-      v-if="currentHit"
-      initialLayout="vertical"
-      :video="currentHit.video"
-      :ref="`youtube-${hitIndex}`"
-      :highlight="terms"
-      :hsk="level"
-      :speed="speed"
-      :startLineIndex="startLineIndex"
-      :showFullscreenToggle="false"
-      :autoload="true"
-      :autoplay="navigated"
-      :showLineList="false"
-    />
-    <b-modal
-      ref="playlist-modal"
-      size="lg"
-      centered
-      hide-footer
-      title="Video Caption Search Results"
-      body-class="playlist-modal-wrapper"
-    >
+    <LazyYouTubeWithTranscript v-if="currentHit" initialLayout="vertical" :video="currentHit.video"
+      :ref="`youtube-${hitIndex}`" :highlight="terms" :hsk="level" :speed="speed" :startLineIndex="startLineIndex"
+      :showFullscreenToggle="false" :autoload="true" :autoplay="navigated" :showLineList="false" />
+    <b-modal ref="playlist-modal" size="lg" centered hide-footer title="Video Caption Search Results"
+      body-class="playlist-modal-wrapper" @show="onPlaylistModalShown">
       <div class="playlist-modal">
         <div class="pt-3 pl-3 pr-3">
-          <button
-            :class="{
-              'btn btn-small': true,
-              'bg-dark': sort === 'length',
-              'text-white': sort === 'length',
-            }"
-            @click.stop.prevent="sort = 'length'"
-          >
+          <button :class="{
+            'btn btn-small': true,
+            'bg-dark': sort === 'length',
+            'text-white': sort === 'length',
+          }" @click.stop.prevent="sort = 'length'">
             Sort By Length
           </button>
-          <button
-            :class="{
-              'btn btn-small': true,
-              'bg-dark': sort === 'left',
-              'text-white': sort === 'left',
-            }"
-            @click.stop.prevent="sort = 'left'"
-          >
+          <button :class="{
+            'btn btn-small': true,
+            'bg-dark': sort === 'left',
+            'text-white': sort === 'left',
+          }" @click.stop.prevent="sort = 'left'">
             Sort Left
           </button>
-          <button
-            :class="{
-              'btn btn-small': true,
-              'bg-dark': sort === 'right',
-              'text-white': sort === 'right',
-            }"
-            @click.stop.prevent="sort = 'right'"
-          >
+          <button :class="{
+            'btn btn-small': true,
+            'bg-dark': sort === 'right',
+            'text-white': sort === 'right',
+          }" @click.stop.prevent="sort = 'right'">
             Sort Right
           </button>
         </div>
         <template v-for="c in get(`groupIndex${ucFirst(sort)}`)">
-          <div
-            :set="(theseHits = get(`groups${ucFirst(sort)}`)[c])"
-            :key="`comp-subs-grouping-${sort}-${c}`"
-          >
-            <hr
-              :key="`comp-subs-grouping-${c}-divider`"
-              v-if="theseHits && theseHits.length > 0"
-            />
+          <div :set="(theseHits = get(`groups${ucFirst(sort)}`)[c])" :key="`comp-subs-grouping-${sort}-${c}`">
+            <hr :key="`comp-subs-grouping-${c}-divider`" v-if="theseHits && theseHits.length > 0" />
             <template v-for="(hit, index) in theseHits">
-              <div
-                @click.stop="goToHit(hit)"
-                :class="{
-                  current: hit === currentHit,
-                  'playlist-modal-item': true,
-                }"
-                :key="`dropdown-line-${c}-${index}`"
-              >
-                <SmallStar
-                  :item="hit"
-                  :saved="(hit) => hit.saved"
-                  :save="saveHit"
-                  :remove="removeSavedHit"
-                />
-                <img
-                  class="hit-thumb"
-                  :src="`//img.youtube.com/vi/${hit.video.youtube_id}/hqdefault.jpg`"
-                  :alt="hit.video.title"
-                  v-lazy-load
-                />
-                <span
-                  :key="`dropdown-line-${index}-annotate-${
-                    hit.video.subs_l2[Number(hit.lineIndex)].line
-                  }`"
-                >
+              <div @click.stop="goToHit(hit)" :class="{
+                current: hit === currentHit,
+                'playlist-modal-item': true,
+              }" :key="`dropdown-line-${c}-${index}`"
+                :ref="hit === currentHit ? 'playlist-modal-current-item' : undefined">
+                <SmallStar :item="hit" :saved="(hit) => hit.saved" :save="saveHit" :remove="removeSavedHit" />
+                <img class="hit-thumb" :src="`//img.youtube.com/vi/${hit.video.youtube_id}/hqdefault.jpg`"
+                  :alt="hit.video.title" v-lazy-load />
+                <span :key="`dropdown-line-${index}-annotate-${hit.video.subs_l2[Number(hit.lineIndex)].line
+                }`">
                   <span>
-                    <span
-                      v-if="
-                        ['left', 'length'].includes(sort) && hit.lineIndex > 0
-                      "
-                      v-html="hit.video.subs_l2[Number(hit.lineIndex) - 1].line"
-                      style="margin-right: 0.5em; opacity: 0.5"
-                    ></span>
-                    <span
-                      v-html="
-                        highlightMultiple(
-                          hit.video.subs_l2[Number(hit.lineIndex)].line,
-                          terms.map((term) => term),
-                          level
-                        )
-                      "
-                    ></span>
-                    <span
-                      v-if="
-                        sort === 'right' &&
-                        hit.lineIndex < hit.video.subs_l2.length - 1
-                      "
-                      v-html="hit.video.subs_l2[Number(hit.lineIndex) + 1].line"
-                      style="margin-left: 0.5em; opacity: 0.5"
-                    ></span>
+                    <span v-if="
+                      ['left', 'length'].includes(sort) && hit.lineIndex > 0
+                    " v-html="hit.video.subs_l2[Number(hit.lineIndex) - 1].line"
+                      style="margin-right: 0.5em; opacity: 0.5"></span>
+                    <span v-html="
+                      highlightMultiple(
+                        hit.video.subs_l2[Number(hit.lineIndex)].line,
+                        terms.map((term) => term),
+                        level
+                      )
+                    "></span>
+                    <span v-if="
+                      sort === 'right' &&
+                      hit.lineIndex < hit.video.subs_l2.length - 1
+                    " v-html="hit.video.subs_l2[Number(hit.lineIndex) + 1].line"
+                      style="margin-left: 0.5em; opacity: 0.5"></span>
                   </span>
                 </span>
               </div>
@@ -462,6 +339,16 @@ export default {
     },
     showPlaylistModal() {
       this.$refs["playlist-modal"].show();
+      // let element = document.querySelector('.playlist-modal-item.current')
+      // console.log(element)
+      // if (element) element.scrollIntoView()
+    },
+    async onPlaylistModalShown() {
+      await Helper.timeout(500)
+      if (this.$refs["playlist-modal-current-item"]) {
+        let ref = this.$refs["playlist-modal-current-item"][0]
+        ref.scrollIntoView({ block: "center" })
+      }
     },
     hitIndexFromSlideIndex(slideIndex) {
       let currentSlideIndex = this.slideIndex;
@@ -487,7 +374,7 @@ export default {
         response = await axios.delete(
           `${Config.youtubeVideosTableName(this.$l2.id)}/${id}`
         );
-      } catch (err) {}
+      } catch (err) { }
       let hits = [];
       for (let hit of this.hits) {
         if (hit !== this.currentHit && hit.video.id !== id) {
@@ -841,15 +728,19 @@ export default {
     margin-bottom: -0.52rem;
     overflow: hidden;
   }
+
   .btn {
     margin: 0;
   }
+
   .btn:disabled {
     opacity: 0.2;
   }
+
   .video-area {
     background: black;
   }
+
   &.fullscreen {
     position: fixed;
     left: 0;
@@ -860,13 +751,16 @@ export default {
     overflow: scroll;
     margin-top: 0 !important;
   }
+
   &.search-subs-light.fullscreen {
     background: white;
   }
+
   &.search-subs-dark {
     &.fullscreen {
       background: black;
     }
+
     .search-subs-hit-index {
       color: rgba(255, 255, 255, 0.877);
     }
