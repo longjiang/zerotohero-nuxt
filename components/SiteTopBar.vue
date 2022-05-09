bf21bb981adc043a6d47dd634ec76ea45238306a
Jiang Long <longjiang2005@gmail.com>
2022-05-09 13:42:50 -0700

    Site top bar should only show additional elements if it's wide enough.

--- components/SiteTopBar.vue
+++ components/SiteTopBar.vue
@@ -1,48 +1,111 @@
 <template>
-  <div :class="`site-top-bar site-top-bar-${variant}`" @click.self="scrollToTop">
-    <template >
-      <div class="text-center">
-        <span style="line-height: 2.3rem; color: #ccc; cursor: pointer; margin-right: 0.5rem" @click="variant === 'side-bar' ? toggleCollapse: scrollToTop"><i
-            class="fas fa-bars"></i></span>
-        <router-link to="/" class="link-unstyled">
-          <img v-if="!$l2 || $l2.code !== 'zh'" src="/img/czh-icon.png" style="height: 1.5rem; margin-right: 0.25rem" /><b
-            class="text-white" style="font-size: 0.9em" data-not-lazy>zerotohero.ca</b>
-        </router-link>
-        <router-link v-if="$l2" :to="{ name: 'home' }" class="link-unstyled language-flag-and-name"> <i
-            class="fas fa-chevron-right"
-            style="opacity: 0.5; font-size: 0.7em; margin-left: 0.15rem; margin-right: 0.15rem"></i><span
-            style="font-weight: bold; color: white;">{{
-                $l2.code
-            }}</span> <span v-if="flagIcon">{{ flagIcon }}</span>
-        </router-link>
-      </div>
-      <div>
-        <AnnotationSettings v-if="$l2" variant="toolbar" style="position: relative; bottom: -0.1rem;" />
-        <router-link :to="languageMapPath" class="btn top-bar-button btn-unstyled link-unstyled">
-          <i class="fas fa-globe-asia"></i>
-        </router-link>
-        <button :class="[
-          'btn top-bar-button btn-unstyled',
-          { 'd-none': !isPWA || !canShare() },
-        ]" @click="share" style="color: #ccc">
-          <i class="fa fa-share"></i>
-        </button>
-        <button :class="['btn top-bar-button btn-unstyled', { 'd-none': !isPWA }]" @click="reload" style="color: #ccc">
-          <i class="fas fa-sync-alt"></i>
-        </button>
-        <LoginButton v-if="$l2" class="d-inline-block" :icon="true" :text="false" style="color: #ddd" />
-      </div>
-    </template>
-  </div>
+  <container-query :query="query" v-model="params">
+    <div
+      :class="`site-top-bar site-top-bar-${variant}`"
+      @click.self="scrollToTop"
+    >
+      <template>
+        <div class="text-center">
+          <span
+            style="
+              line-height: 2.3rem;
+              color: #ccc;
+              cursor: pointer;
+              margin-right: 0.5rem;
+            "
+            @click="variant === 'side-bar' ? toggleCollapse : scrollToTop"
+          >
+            <i class="fas fa-bars"></i>
+          </span>
+          <router-link to="/" class="link-unstyled">
+            <img
+              v-if="!$l2 || $l2.code !== 'zh'"
+              src="/img/czh-icon.png"
+              style="height: 1.5rem; margin-right: 0.25rem"
+            />
+            <b class="text-white" style="font-size: 0.9em" data-not-lazy>
+              zerotohero.ca
+            </b>
+          </router-link>
+          <router-link
+            v-if="$l2 && params.lg"
+            :to="{ name: 'home' }"
+            class="link-unstyled language-flag-and-name"
+          >
+            <i
+              class="fas fa-chevron-right"
+              style="
+                opacity: 0.5;
+                font-size: 0.7em;
+                margin-left: 0.15rem;
+                margin-right: 0.15rem;
+              "
+            ></i>
+            <span style="font-weight: bold; color: white">{{ $l2.code }}</span>
+            <span v-if="flagIcon">{{ flagIcon }}</span>
+          </router-link>
+        </div>
+        <div>
+          <AnnotationSettings
+            v-if="$l2 && params.lg"
+            variant="toolbar"
+            style="position: relative; bottom: -0.1rem"
+          />
+          <router-link
+            :to="languageMapPath"
+            class="btn top-bar-button btn-unstyled link-unstyled"
+          >
+            <i class="fas fa-globe-asia"></i>
+          </router-link>
+          <button
+            :class="[
+              'btn top-bar-button btn-unstyled',
+              { 'd-none': !isPWA || !canShare() },
+            ]"
+            @click="share"
+            style="color: #ccc"
+          >
+            <i class="fa fa-share"></i>
+          </button>
+          <button
+            :class="['btn top-bar-button btn-unstyled', { 'd-none': !isPWA }]"
+            @click="reload"
+            style="color: #ccc"
+          >
+            <i class="fas fa-sync-alt"></i>
+          </button>
+          <LoginButton
+            v-if="$l2"
+            class="d-inline-block"
+            :icon="true"
+            :text="false"
+            style="color: #ddd"
+          />
+        </div>
+      </template>
+    </div>
+  </container-query>
 </template>
 <script>
 import AnnotationSettings from "./AnnotationSettings.vue";
+import { ContainerQuery } from "vue-container-query";
 
 export default {
+  components: { AnnotationSettings, ContainerQuery },
+  data() {
+    return {
+      params: {},
+      query: {
+        lg: {
+          minWidth: 320,
+        },
+      },
+    };
+  },
   props: {
     variant: {
-      default: "menu-bar"
-    }
+      default: "menu-bar",
+    },
   },
   computed: {
     $l1() {
@@ -54,9 +117,11 @@ export default {
         return this.$store.state.settings.l2;
     },
     isPWA() {
-      return ((typeof navigator !== "undefined" && navigator.standalone) ||
+      return (
+        (typeof navigator !== "undefined" && navigator.standalone) ||
         (typeof window !== "undefined" &&
-          window.matchMedia("(display-mode: standalone)").matches));
+          window.matchMedia("(display-mode: standalone)").matches)
+      );
     },
     languageMapPath() {
       if (this.fullHistory) {
@@ -67,21 +132,20 @@ export default {
           }
         });
         let path = historyMatches.pop();
-        if (path)
-          return path;
+        if (path) return path;
       }
       return "/language-map";
     },
     flagIcon() {
-      return this.$languages.flagIcon(this.$l2)
-    }
+      return this.$languages.flagIcon(this.$l2);
+    },
   },
   methods: {
     toggleCollapse() {
-      this.$emit('toggleCollapse')
+      this.$emit("toggleCollapse");
     },
     scrollToTop() {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({ top: 0, behavior: "smooth" });
     },
     canShare() {
       return typeof navigator !== "undefined" && navigator.share;
@@ -97,8 +161,7 @@ export default {
       location.reload();
     },
   },
-  components: { AnnotationSettings }
-}
+};
 </script>
 <style lang="scss" scoped>
 .site-top-bar {
@@ -129,7 +192,6 @@ export default {
     top: 0;
     z-index: 3;
   }
-
 
   .language-flag-and-name {
     line-height: 1;
