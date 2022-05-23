a96c6dcdd76224954d2803fd8aaebd99adb3093f
Jiang Long <longjiang2005@gmail.com>
2022-05-23 15:06:48 -0700

    Settings adminMode not matching role (string as number)

--- pages/settings.vue
+++ pages/settings.vue
@@ -126,7 +126,7 @@ export default {
         return this.$store.state.settings.l2;
     },
     userIsAdmin() {
-      return this.$auth.user && this.$auth.user.role === 1;
+      return this.$auth.user && this.$auth.user.role == 1;
     },
     ...mapState("settings", ["l2Settings", "l1", "l2"]),
   },
