e928c2f65fb51ca40c63120b758acf5c625ae6f1
Jiang Long <longjiang2005@gmail.com>
2022-05-26 12:56:39 -0700

    Add a page route to display any pages on directus, (e.g. privacy policy)

---
+++ pages/page.vue
@@ -0,0 +1,55 @@
+<router>
+  {
+    path: '/:l1/:l2/page/:id/:title?',
+    props: true
+  }
+</router>
+<template>
+  <div class="main page pt-5 pb-5">
+    <div class="container" v-if="page">
+      <div class="row">
+        <div class="col-sm-12">
+          <h3 class="mb-5">{{ page.title }}</h3>
+          <div v-html="marked" ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Marked from "marked";
+import Config from "@/lib/config";
+
+export default {
+  props: {
+    id: [Number, String],
+  },
+  data() {
+    return {
+      page: undefined,
+    };
+  },
+  computed: {
+    marked() {
+      if (this.page && this.page.body)
+        return (
+          Marked(this.page.body.replace(/^ {4,}/gm, "")) || this.page.body // We remove any FOUR CONSECUTIVE SPACES because they would emit <code>!
+        );
+    },
+  },
+  async created() {
+    try {
+      let res = await this.$authios.get(`${Config.wiki}items/pages/${this.id}`);
+      if (res && res.data && res.data.data) {
+        this.page = res.data.data;
+      }
+    } catch (err) {
+      Helper.logError(err);
+    }
+  },
+};
+</script>
+
+<style>
+</style>
