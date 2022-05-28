4124e0d652c8dc86d7f9f74d310882454c323b23
Jiang Long <longjiang2005@gmail.com>
2022-05-28 15:21:52 -0700

    Show.vue: fix mistake in last commit.

--- pages/show.vue
+++ pages/show.vue
@@ -372,7 +372,7 @@ export default {
     },
     async getVideos({ keyword, limit = this.perPage, offset = 0, sort = "title" } = {}) {
       limit = Math.min(limit, this.episodeCount - offset)
-      if (this.show.episodes && this.show.episodes.length > offset + limit) return this.show.episodes.slice(offset, limit);
+      if (this.show.episodes && this.show.episodes.length >= offset + limit) return this.show.episodes.slice(offset, limit);
       else {
         return await this.getVideosFromServer({ keyword, limit, offset, sort });
       }
