49ff202c772d73e8ff030d3a1ae0e720e3c28873
Jiang Long <longjiang2005@gmail.com>
2022-06-01 00:06:32 -0700

    Move review quiz to the very end.

---
+++ components/EndQuiz.vue
@@ -0,0 +1,209 @@
+<template>
+  <div class="review-wrapper" v-observe-visibility="visibilityChanged">
+    <div>
+      <Review
+        v-for="(reviewItem, reviewItemIndex) in reviewFiltered"
+        :key="`review-${reviewItemIndex}`"
+        :reviewItem="reviewItem"
+        :hsk="hsk"
+        :skin="skin"
+      />
+      <div class="text-center mt-2" v-if="review.length > 1">
+        <span
+          class="d-inline-block"
+          style="
+            position: relative;
+            bottom: 0.13rem;
+            font-weight: normal;
+            opacity: 0.5;
+            font-size: 0.8em;
+            cursor: pointer;
+          "
+        >
+          <span v-if="!reviewOpen" @click="reviewOpen = true">
+            <i class="fa fa-chevron-down mr-1"></i>
+            Show More Questions
+          </span>
+          <span v-if="reviewOpen === true" @click="reviewOpen = false">
+            <i class="fa fa-chevron-up mr-1"></i>
+            Show Fewer Questions
+          </span>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import Helper from "@/lib/helper";
+
+export default {
+  props: {
+    lines: {
+      type: Array,
+    },
+    matchedParallelLines: {
+      type: Array,
+    },
+    hsk: {
+      type: [Number, String],
+    },
+    skin: {
+      default: "light",
+    },
+  },
+  data() {
+    return {
+      active: false, // activate only when visible the first time
+      review: [],
+      reviewOpen: false,
+    };
+  },
+  computed: {
+    ...mapState("savedWords", ["savedWords"]),
+    $l1() {
+      if (typeof this.$store.state.settings.l1 !== "undefined")
+        return this.$store.state.settings.l1;
+    },
+    $l2() {
+      if (typeof this.$store.state.settings.l2 !== "undefined")
+        return this.$store.state.settings.l2;
+    },
+    $adminMode() {
+      if (typeof this.$store.state.settings.adminMode !== "undefined")
+        return this.$store.state.settings.adminMode;
+    },
+    quiz() {
+      if (typeof this.$store.state.settings.l2Settings !== "undefined")
+        return this.$store.state.settings.l2Settings.showQuiz;
+      else return false;
+    },
+    reviewFiltered() {
+      let reviewFiltered = this.review.slice(
+        0,
+        this.reviewOpen ? this.review.length : 1
+      );
+      return reviewFiltered;
+    },
+  },
+  beforeDestroy() {
+    if (this.unsubscribe) this.unsubscribe();
+  },
+  created() {
+    this.unsubscribe = this.$store.subscribe((mutation) => {
+      this.updateReview(mutation);
+    });
+  },
+  methods: {
+    /**
+     * Generate review items during initial load.
+     */
+    async visibilityChanged(visible) {
+      if (visible) {
+        if (
+          !this.active &&
+          this.savedWords &&
+          this.savedWords[this.$l2.code] &&
+          this.review.length === 0
+        ) {
+          this.review = await this.generateReview();
+        }
+        this.active = true;
+      }
+    },
+    async generateReview() {
+      let reviewItems = [];
+      for (let savedWord of this.savedWords[this.$l2.code]) {
+        let word = await (await this.$getDictionary()).get(savedWord.id);
+        if (word) {
+          reviewItems = reviewItems.concat(
+            await this.reviewItemsForWord(word, savedWord.forms)
+          );
+        }
+      }
+      return reviewItems;
+    },
+    reviewConditions(lineIndex, form, word) {
+      let line = this.lines[lineIndex];
+      if (["en", "ru"].includes(this.$l2.code)) {
+        return line.line.includes(form);
+      }
+      if (
+        (this.$l2.continua || this.$l2.agglutinative) &&
+        line.line.includes(form)
+      )
+        return true;
+      if (
+        this.$l2.han &&
+        (line.line.includes(word.simplified) ||
+          line.line.includes(word.traditional))
+      )
+        return true;
+      if (!this.$l2.continua) {
+        form = Helper.escapeRegExp(form);
+        let found = false;
+        try {
+          found =
+            new RegExp(`[ .,:!?]${form}[ .,:!?]`, "gi").test(line.line) ||
+            new RegExp(`^${form}[ .,:!?]`, "gi").test();
+        } catch (err) {}
+        return found;
+      }
+    },
+    async reviewItemsForWord(word, wordForms) {
+      let reviewItems = [];
+      let forms = wordForms.filter((form) => form && form !== "-");
+      forms = Helper.uniqueIgnoreCase(forms);
+      for (let form of forms.sort((a, b) => b.length - a.length)) {
+        for (let lineIndex in this.lines) {
+          if (this.reviewConditions(lineIndex, form, word)) {
+            let reviewItem = await this.generateReviewItem(
+              lineIndex,
+              form,
+              word
+            );
+            reviewItems.push(reviewItem);
+          }
+        }
+      }
+      return reviewItems;
+    },
+    async generateReviewItem(lineIndex, form, word) {
+      let line = this.lines[lineIndex];
+      let parallelLines = this.matchedParallelLines
+        ? this.matchedParallelLines[lineIndex]
+        : undefined;
+      return {
+        line,
+        lineIndex,
+        parallelLines,
+        text: form,
+        word,
+        simplified: word.simplified,
+        traditional: word.traditional,
+      };
+    },
+    async updateReview(mutation) {
+      if (mutation.type === "savedWords/LOAD") {
+        if (this.review.length === 0) this.review = await this.generateReview()
+      }
+      if (mutation.type === "savedWords/ADD_SAVED_WORD") {
+        this.review = this.review.concat(
+          await this.reviewItemsForWord(
+            mutation.payload.word,
+            mutation.payload.wordForms
+          )
+        );
+      } else if (mutation.type === "savedWords/REMOVE_SAVED_WORD") {
+        this.review = this.review.filter(
+          (reviewItem) => mutation.payload.word.id !== reviewItem.word.id
+        );
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- components/SyncedTranscript.vue
+++ components/SyncedTranscript.vue
@@ -63,73 +63,6 @@
           @trasnlationLineBlur="trasnlationLineBlur"
           @trasnlationLineKeydown="trasnlationLineKeydown"
         />
-        <div
-          class="review-placeholder"
-          :key="`line-${index + visibleMin}-review-placeholder-${
-            reviewKeys[index + visibleMin]
-          }`"
-        >
-          <div
-            v-if="!single && review[index + visibleMin]"
-            :key="`line-${index + visibleMin}-review-${
-              reviewKeys[index + visibleMin]
-            }`"
-          >
-            <Review
-              v-for="(reviewItem, reviewItemIndex) in review[
-                index + visibleMin
-              ].slice(
-                0,
-                reviewOpen[index + visibleMin] === true
-                  ? reviewOpen[index + visibleMin].length
-                  : 1
-              )"
-              :key="`review-${index + visibleMin}-${
-                reviewItem.text || reviewItem.simplified
-              }-${reviewItemIndex}-${reviewKeys[index + visibleMin]}`"
-              :reviewItem="reviewItem"
-              :hsk="hsk"
-              :skin="skin"
-            />
-            <div
-              class="text-center mt-2"
-              :key="`review-title-${index + visibleMin}-${
-                reviewKeys[index + visibleMin]
-              }`"
-              v-if="
-                review[index + visibleMin] &&
-                review[index + visibleMin].length > 1
-              "
-            >
-              <span
-                class="d-inline-block"
-                style="
-                  position: relative;
-                  bottom: 0.13rem;
-                  font-weight: normal;
-                  opacity: 0.5;
-                  font-size: 0.8em;
-                  cursor: pointer;
-                "
-              >
-                <span
-                  v-if="!reviewOpen[index + visibleMin]"
-                  @click="openReview(index + visibleMin)"
-                >
-                  <i class="fa fa-chevron-down mr-1"></i>
-                  Show More Questions
-                </span>
-                <span
-                  v-if="reviewOpen[index + visibleMin] === true"
-                  @click="closeReview(index + visibleMin)"
-                >
-                  <i class="fa fa-chevron-up mr-1"></i>
-                  Show Fewer Questions
-                </span>
-              </span>
-            </div>
-          </div>
-        </div>
       </template>
       <div
         v-observe-visibility="visibilityChanged"
@@ -143,6 +76,7 @@
       >
         <Loader :sticky="true" />
       </div>
+      <EndQuiz :lines="lines" :matchedParallelLines="matchedParallelLines" :hsk="hsk"  :skin="skin" />
     </client-only>
   </div>
 </template>
@@ -213,16 +147,12 @@ export default {
       currentTime: 0,
       currentLine: undefined,
       currentLineIndex: undefined,
-      reviewLineOffset: 10, // Show review this number of lines after the first appearance of the word
       nextLine: undefined,
-      review: {},
       paused: true,
       ended: false,
       audioMode: false,
       repeatMode: false,
       audioCancelled: false,
-      reviewKeys: [],
-      reviewOpen: [],
       neverPlayed: true,
       matchedParallelLines: undefined,
       visibleMin: 0,
@@ -253,11 +183,6 @@ export default {
       if (typeof this.$store.state.settings.adminMode !== "undefined")
         return this.$store.state.settings.adminMode;
     },
-    quiz() {
-      if (typeof this.$store.state.settings.l2Settings !== "undefined")
-        return this.$store.state.settings.l2Settings.showQuiz;
-      else return false;
-    },
     previousLine() {
       let previousIndex = Math.max(this.currentLineIndex - 1, 0);
       return this.lines && this.lines[previousIndex]
@@ -271,38 +196,15 @@ export default {
       if (line.duration) Number(line.duration);
     });
     if (this.parallellines) this.matchParallelLines();
-    this.reviewKeys = this.lines.map(() => 0);
-    if (this.quiz && this.highlightSavedWords) {
-      if (
-        this.$store.state.savedWords.savedWords &&
-        this.$store.state.savedWords.savedWords[this.$l2.code]
-      ) {
-        this.review = await this.generateReview();
-      }
-      this.unsubscribe = this.$store.subscribe((mutation, state) => {
-        if (mutation.type.startsWith("savedWords")) {
-          this.updateReview(mutation);
-        }
-      });
-    }
     if (this.startLineIndex) {
       let startLineIndex = Number(this.startLineIndex);
       this.currentLine = this.lines[startLineIndex];
       this.currentLineIndex = startLineIndex;
       this.nextLine = this.lines[startLineIndex + 1];
     }
-    // if (process.client) {
-    //   window.addEventListener("wheel", this.cancelSmoothScroll);
-    //   window.addEventListener("touchstart", this.cancelSmoothScroll);
-    // }
     this.attachPopupEventListeners();
   },
   beforeDestroy() {
-    if (this.unsubscribe) this.unsubscribe();
-    // if (process.client) {
-    //   window.removeEventListener("wheel", this.cancelSmoothScroll);
-    //   window.removeEventListener("touchstart", this.cancelSmoothScroll);
-    // }
     this.$nuxt.$off("popupOpened");
     this.$nuxt.$off("popupClosed");
   },
@@ -395,12 +297,6 @@ export default {
       while (id--) {
         window.cancelAnimationFrame(id);
       }
-    },
-    openReview(index) {
-      Vue.set(this.reviewOpen, index, true);
-    },
-    closeReview(index) {
-      Vue.set(this.reviewOpen, index, undefined);
     },
     async turnOffPreventJumptingAtStartAfter3Seconds() {
       await Helper.timeout(3000);
@@ -519,54 +415,34 @@ export default {
       if (!this.currentLineIndex) {
         this.currentTime = this.currentTime + 0.1;
       }
-      // console.log("🍉 started do audio stuff");
       this.$emit("pause");
       this.audioCancelled = false;
       if (
         this.matchedParallelLines &&
         this.matchedParallelLines[this.currentLineIndex]
       ) {
-        // await Helper.timeout(1000);
         this.$emit("speechStart");
         let englishPromise = Helper.speak(
           this.matchedParallelLines[this.currentLineIndex],
           this.$l1,
           1.1
         );
-        // console.log(englishPromise, "englishPromise");
         await englishPromise;
-        // console.log("🇺🇸 english finished");
       }
       if (!this.audioCancelled && !window.speechSynthesis.speaking) {
         if (this.currentLine) {
-          // await Helper.timeout(1000);
-          // console.log("🇯🇵 japanese finished");
           this.$emit("speechStart");
           let japanesePromise = Helper.speak(
             this.currentLine.line,
             this.$l2,
             1
           );
-          // console.log(japanesePromise, "japanesePromise");
           await japanesePromise;
-          // console.log("🇯🇵 japanese finished");
-          // console.log("📺 resuming");
-          if (this.quiz && this.review[this.currentLineIndex]) {
-            await Helper.speak(
-              "Please complete the pop quiz",
-              this.$l1,
-              1.1,
-              0.2
-            );
-          } else {
-            this.$emit("play");
-          }
           this.$emit("speechEnd");
         }
       }
     },
     stopAudioModeStuff() {
-      // console.log("🤚 stopping audio stuff");
       this.audioCancelled = true;
       window.speechSynthesis.cancel();
       this.$emit("speechEnd");
@@ -577,131 +453,6 @@ export default {
       if (this.matchedParallelLines)
         this.matchedParallelLines.splice(lineIndex, 1);
       this.emitUpdateTranslation();
-    },
-    /**
-     * Generate review items during initial load.
-     */
-    async generateReview() {
-      let review = {};
-      let affectedLineNumbers = [];
-      for (let savedWord of this.$store.state.savedWords.savedWords[
-        this.$l2.code
-      ]) {
-        let word = await (await this.$getDictionary()).get(savedWord.id);
-        if (word) {
-          let lineNumbers = await this.addReviewItemsForWord(
-            word,
-            savedWord.forms,
-            review
-          );
-          affectedLineNumbers = affectedLineNumbers.concat(lineNumbers);
-        }
-      }
-      this.updateKeysForLines(affectedLineNumbers);
-      return review;
-    },
-    async updateReview(mutation) {
-      if (mutation.type === "savedWords/ADD_SAVED_WORD") {
-        let affectedLines = await this.addReviewItemsForWord(
-          mutation.payload.word,
-          mutation.payload.wordForms,
-          this.review
-        );
-        this.updateKeysForLines(affectedLines);
-      } else if (mutation.type === "savedWords/REMOVE_SAVED_WORD") {
-        let affectedLines = this.removeReviewItemsForWord(
-          mutation.payload.word,
-          this.review
-        );
-        this.updateKeysForLines(affectedLines);
-      }
-    },
-    async addReviewItemsForWord(word, wordForms, review) {
-      let lineOffset = this.reviewLineOffset;
-      let affectedLineNumbers = [];
-      let forms = wordForms.filter((form) => form && form !== "-");
-      forms = Helper.uniqueIgnoreCase(forms);
-      for (let form of forms.sort((a, b) => b.length - a.length)) {
-        for (let lineIndex in this.lines) {
-          if (this.reviewConditions(lineIndex, form, word)) {
-            let reviewItem = await this.generateReviewItem(
-              lineIndex,
-              form,
-              word
-            );
-            let reviewIndex = Math.min(
-              Math.ceil((Number(lineIndex) + lineOffset) / 10) * 10,
-              this.lines.length - 1
-            );
-            review[reviewIndex] = review[reviewIndex] || [];
-            review[reviewIndex].push(reviewItem);
-            affectedLineNumbers.push(reviewIndex);
-            this.reviewKeys[reviewIndex]++;
-          }
-        }
-      }
-      return affectedLineNumbers;
-    },
-    updateKeysForLines(affectedLines) {
-      for (let lineIndex in this.reviewKeys) {
-        if (affectedLines.includes(Number(lineIndex))) {
-          Vue.set(this.reviewKeys, lineIndex, this.reviewKeys[lineIndex] + 1);
-        }
-      }
-    },
-    removeReviewItemsForWord(word, review) {
-      let affectedLines = [];
-      for (let reviewIndex in review) {
-        let length = review[reviewIndex].length;
-        review[reviewIndex] = review[reviewIndex].filter(
-          (reviewItem) => word.id !== reviewItem.word.id
-        );
-        if (review[reviewIndex].length < length)
-          affectedLines.push(Number(reviewIndex));
-      }
-      return affectedLines;
-    },
-    reviewConditions(lineIndex, form, word) {
-      let line = this.lines[lineIndex];
-      if (["en", "ru"].includes(this.$l2.code)) {
-        return line.line.includes(form);
-      }
-      if (
-        (this.$l2.continua || this.$l2.agglutinative) &&
-        line.line.includes(form)
-      )
-        return true;
-      if (
-        this.$l2.han &&
-        (line.line.includes(word.simplified) ||
-          line.line.includes(word.traditional))
-      )
-        return true;
-      if (!this.$l2.continua) {
-        form = Helper.escapeRegExp(form);
-        let found = false;
-        try {
-          found =
-            new RegExp(`[ .,:!?]${form}[ .,:!?]`, "gi").test(line.line) ||
-            new RegExp(`^${form}[ .,:!?]`, "gi").test();
-        } catch (err) {}
-        return found;
-      }
-    },
-    async generateReviewItem(lineIndex, form, word) {
-      let line = this.lines[lineIndex];
-      let parallelLines = this.matchedParallelLines
-        ? this.matchedParallelLines[lineIndex]
-        : undefined;
-      return {
-        line,
-        lineIndex,
-        parallelLines,
-        text: form,
-        word,
-        simplified: word.simplified,
-        traditional: word.traditional,
-      };
     },
     seekVideoTo(starttime) {
       this.$emit("seek", starttime);
