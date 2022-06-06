2c19670999986b50e7e96b22cac42ad8c4015def
Jiang Long <longjiang2005@gmail.com>
2022-06-05 19:57:06 -0700

    Feed: when returning to feed, always go back to the same position

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,4 +1,7 @@
+let feedSavedPosition
+
 export default function (to, from, savedPosition) {
+  if (from.name === 'all-media') feedSavedPosition = { x: window.scrollX, y: window.scrollY }
   if (to.hash && to.hash !== '#') {
     return {
       selector: to.hash
@@ -6,7 +9,7 @@ export default function (to, from, savedPosition) {
   } else if (from.name === 'dictionary' && to.name === 'dictionary') {
     return '.dictionary-main'
   } else if (to.name === 'all-media') {
-    if (savedPosition) return savedPosition
+    if (feedSavedPosition) return feedSavedPosition
   } else {
     return { x: 0, y: -40 }
   }
