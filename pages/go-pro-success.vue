b710b52e4b8b69584d336b4dd73dcf1a3b5f9cfc
Jiang Long <longjiang2005@gmail.com>
2022-06-13 02:06:09 -0700

    Go Pro Success page: tweak success page to handle cases where the payment is successful but we can't log the user in.

--- pages/go-pro-success.vue
+++ pages/go-pro-success.vue
@@ -8,8 +8,9 @@
   <div class="main container mt-5 p-4 rounded shadow">
     <div class="row">
       <div class="col-sm-12">
+        <h3>You’re now Pro!</h3>
         <client-only>
-          <div v-if="$auth.loggedIn && $auth.user">
+          <div v-if="$auth.loggedIn && $auth.user && pro">
             <div>
               Welcome,
               <b>{{ $auth.user ? $auth.user.email : "" }}</b>
@@ -19,7 +20,6 @@
             </div>
             <div class="mt-3"></div>
             <div v-if="pro">
-              <h3>You’re now Pro!</h3>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
                 praesentium ex excepturi rem magnam vitae inventore doloribus
@@ -33,14 +33,13 @@
             </div>
           </div>
           <div v-else>
-            <h3>Welcome to Pro!</h3>
             <p>
               Your upgrade was successful. Please login again to activate your Pro features.
             </p>
             <div class="mt-3" />
             <div>
               <router-link
-                :to="{ path: '/login' }"
+                :to="{ path: '/login?redirect=/go-pro-success' }"
                 class="btn btn-success"
               >
                 Login
