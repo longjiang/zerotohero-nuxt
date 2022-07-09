e91ef7d1dd8b57620bcd558ac68553f600195ee0
Jiang Long <longjiang2005@gmail.com>
2022-07-09 12:27:59 -0700

    Subs search: default subs search filter to only tv shows.

--- store/settings.js
+++ store/settings.js
@@ -21,8 +21,8 @@ export const state = () => {
       useSerif: false,
       showQuiz: true,
       showByeonggi: true,
-      tvShowFilter: "all",
-      talkFilter: "all",
+      tvShowFilter: "all", // By default we only search TV shows.
+      talkFilter: [],
       disableAnnotation: false
     },
     romanizationOffByDefault: ["ko", "bo", "dz", "th", "my", "hy", "vi"]
