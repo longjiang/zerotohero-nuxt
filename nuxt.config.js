54b46821c3eed0a9c36a2b475eac4e2dc9e0b47c
Jiang Long <longjiang2005@gmail.com>
2022-07-14 03:23:26 -0700

    Change head title.

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Zero to Hero Languages',
+    title: 'Language Player',
     htmlAttrs: {
       lang: 'en'
     },
