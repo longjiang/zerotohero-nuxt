4bcd176498ad2267706e4e11647f4027b7cf5dd9
Jiang Long <longjiang2005@gmail.com>
2022-08-06 01:28:10 -0700

    Add legend to language map.

--- pages/language-map.vue
+++ pages/language-map.vue
@@ -9,16 +9,55 @@
   }
 </router>
 <template>
-  <div style="display: flex; flex-direction: column; height: 100vh;">
+  <div style="display: flex; flex-direction: column; height: 100vh">
     <SocialHead
       title="Map of World Languages | Language Player"
       description="Tap on any language label to learn the language! Live TV channels, TV shows with subtitles, music with lyrics, phrasebooks with video examples... everything that can help you to learn a language “by osmosis.”"
       image="/img/thumbnail-language-map.jpg"
     />
-    <SiteTopBar class="safe-padding-top"/>
+    <SiteTopBar class="safe-padding-top" />
+    <div
+      :class="{
+        'language-map-description': true,
+        'language-map-description-collapsed': hideDescription,
+      }"
+    >
+      <b-button
+        variant="unstyled"
+        class="btn-close"
+        @click="hideDescription = !hideDescription"
+      >
+        <i class="fa-solid fa-xmark" v-if="!hideDescription"></i>
+        <span v-else>Show Legend</span>
+      </b-button>
+      <h5 class="text-white">World Language Map</h5>
+      <p class="text-white">
+        <i class="fa-solid fa-arrow-pointer"></i>
+        Click on any language to learn it.
+      </p>
+      <p>
+        This is an interactive map of 6,000 languages of the world, including
+        4,000 living languages, 2,000 historic languages, and 6 constructed
+        languages.
+      </p>
+      <p>
+        <b class="text-white">Legend.</b>
+        Circle sizes correspond to speaker populations. Circles are colour-coded
+        by language families.
+      </p>
+      <ul class="list-unstyled">
+        <li v-for="family in langFamilies" :key="`legend-family-${family.id}`">
+          <span
+            class="legend-dot"
+            :style="`background-color: ${family.color};`"
+          ></span>
+          <span>{{ family.name }}</span>
+        </li>
+      </ul>
+    </div>
     <div class="container-fluid flex-1">
       <div class="row">
-        <div class="col-12 p-0" >
+        <div class="col-12 p-0">
           <div class="loader-wrapper" v-if="loadingMap">
             <Loader
               :sticky="true"
@@ -43,7 +82,7 @@
               :key="`language-map-${languagesKey}`"
               :l1="l1"
               @ready="onReady"
-               style="height: 100vh"
+              style="height: 100vh"
             />
           </client-only>
         </div>
@@ -58,10 +97,24 @@ export default {
   data() {
     return {
       l1: "en",
+      hideDescription: false,
       loadingMap: true,
       filteredLangsWithGeo: undefined,
       filteredLangs: undefined,
       languagesKey: 0,
+      langFamilies: [
+        { id: "afro1255", name: "Afro-Asiatic", color: "#f8b51e" },
+        { id: "atla1278", name: "Atlantic-Congo", color: "#fd4f1c" },
+        { id: "aust1305", name: "Austroasiatic", color: "#5b0516" },
+        { id: "aust1307", name: "Austronesian", color: "#6a3669" },
+        { id: "drav1251", name: "Dravidian", color: "#28a745" },
+        { id: "indo1319", name: "Indo-European", color: "#1b3e76" },
+        { id: "nucl1709", name: "Nuclear Trans New Guinea", color: "#0076ba" },
+        { id: "sino1245", name: "Sino-Tibetan", color: "#bb1718" },
+        { id: "taik1256", name: "Tai-Kadai", color: "#b1c751" },
+        { id: "turk1311", name: "Turkic", color: "#005f58" },
+        { id: "", name: "Others", color: "#000" },
+      ],
     };
   },
   /**
@@ -76,10 +129,13 @@ export default {
     },
   },
   async mounted() {
-    this.$store.commit('settings/SET_L1_L2_TO_NULL')
-    await this.$languages.loadFull()
+    this.$store.commit("settings/SET_L1_L2_TO_NULL");
+    await this.$languages.loadFull();
     await Helper.timeout(100);
     this.updateLanguages();
+    if (window && window.innerWidth < 720) {
+      this.hideDescription = true
+    }
   },
   watch: {
     l1() {
@@ -173,5 +229,49 @@ export default {
 ::v-deep .leaflet-right .leaflet-control {
   margin-top: 4rem;
   margin-right: 1rem;
+}
+
+.language-map-description {
+  position: fixed;
+  width: 19.5rem;
+  height: calc(100vh - env(safe-area-inset-top) - env(safe-area-inset-bottom) - 9rem - 1rem);
+  background-color: #000000ee;
+  right: 1rem;
+  border-radius: 0.5rem;
+  top: calc(env(safe-area-inset-top) + 9rem);
+  padding: 1.5rem;
+  z-index: 401;
+  color: #aaa;
+  overflow-y: auto;
+  .btn-close {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    color: #666;
+    font-size: 1.3em;
+  }
+  .legend-dot {
+    height: 1rem;
+    width: 1rem;
+    display: inline-block;
+    border: 1px solid #666;
+    border-radius: 100%;
+    margin-right: 0.5rem;
+    position: relative;
+    bottom: -0.15rem;
+  }
+  &.language-map-description-collapsed {
+    height: auto;
+    width: 5rem;
+    padding: 0.25rem;
+    .btn-close {
+      font-size: .8em;
+      position: static;
+      color: white;
+    }
+    > *:not(button) {
+      display: none;
+    }
+  }
 }
 </style>
