a1135bf9416b552197090a80ac6bd1479933ba80
Jiang Long <longjiang2005@gmail.com>
2022-11-28 01:20:42 -0800

    Add new youtube-import page.

---
+++ pages/youtube-import.vue
@@ -0,0 +1,73 @@
+<router>
+  {
+    path: '/:l1/:l2/youtube/import',
+    props: true,
+    meta: {
+      skin: 'dark',
+    },
+  }
+</router>
+<template>
+  <div class="main main-dark">
+    <div class="container pt-5 pb-5 youtube-import">
+      <div style="font-size: 1.5rem; color: white; text-align: center">
+        <img
+          src="/img/youtube.png"
+          style="height: 4rem; margin-bottom: 1rem"
+          data-not-lazy
+        />
+      </div>
+      <h4 class="mt-3 mb-5 text-center">
+        Import
+        {{ $l2.name }} Videos from YouTube
+      </h4>
+      <b-input-group class="flex-1 input-group-ghost-dark">
+        <b-form-input
+          v-model="url"
+          :lazy="true"
+          @compositionend.prevent.stop="() => false"
+          :placeholder="'URL of a YouTube video, playlist or channel...'"
+          class="input-ghost-dark"
+        />
+      </b-input-group>
+    </div>
+  </div>
+</template>
+
+<script>
+import YouTube from "@/lib/youtube";
+
+export default {
+  data() {
+    return {
+      url: undefined,
+    };
+  },
+  computed: {
+    $l1() {
+      return this.$store.state.settings.l1;
+    },
+    $l2() {
+      return this.$store.state.settings.l2;
+    },
+  },
+  watch: {
+    url() {
+      if (this.url && this.url.length > 0) {
+        let youtubeId, playlistId;
+        youtubeId = YouTube.videoIdFromURL(this.url);
+        playlistId = YouTube.playlistIdFromURL(this.url);
+        if (!youtubeId && !playlistId) {
+          // the user may have simply entered an id
+          if (this.url.startsWith("PL")) playlistId = this.url;
+          else youtubeId = this.url;
+        }
+        console.log({ youtubeId, playlistId });
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
