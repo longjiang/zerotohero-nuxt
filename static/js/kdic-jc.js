d823d84bcf3c6443c515e372afbf0f7f5050adc7
Jiang Long <longjiang2005@gmail.com>
2023-02-14 03:43:38 -0800

    Add the ability to study Japanese from Chinese as L1.

---
+++ static/js/kdic-jc.js
@@ -0,0 +1,318 @@
+importScripts('../vendor/kuromoji/kuromoji.js')
+importScripts('../vendor/wanakana/wanakana.min.js')
+importScripts('../vendor/jpconjugations.js')
+importScripts('../vendor/localforage/localforage.js')
+importScripts("../vendor/hash-string/hash-string.min.js")
+
+const Dictionary = {
+  file: 'https://server.chinesezerotohero.com/data/kdic-jc/kdic-jc.tsv.txt',
+  words: [],
+  tokenizationCache: {},
+  name: 'edict',
+  tokenizer: undefined,
+  credit() {
+    return 'The Japanese dictionary is provided by ...'
+  },
+  async load() {
+    this.tokenizer = await new Promise(resolve => {
+      kuromoji.builder({ dicPath: `https://server.chinesezerotohero.com/data/kuromoji/` }).build((err, tokenizer) => {
+        resolve(tokenizer)
+      })
+    })
+    let [edictData] = await Promise.all([
+      this.loadSmart('kdic-jc', this.file),
+    ]);
+    let words = await this.loadEdict(edictData)
+    this.words = this.uniqueByValues(words, ["id"]);
+    words = null
+    return this
+  },
+
+  async loadEdict(csv) {
+    let results = await Papa.parse(csv, {
+      header: true
+    });
+    let data = results.data
+    let sorted = data.sort((a, b) =>
+      a.kana && b.kana ? a.kana.length - b.kana.length : 0
+    )
+    let words = []
+    for (let row of sorted) {
+      if (row.kanji === 'ー') delete row.kana
+
+      let word = Object.assign(row, {
+        head: row.kanji || row.kana,
+        bare: row.kanji || row.kana,
+        accented: row.kanji || row.kana,
+        pos: '',
+        definitions: row.translation ? row.translation.split('，').filter(d => d !== '') : [],
+        cjk: {
+          canonical: row.kanji && row.kanji !== 'NULL' ? row.kanji : undefined,
+          phonetics: row.kana
+        },
+        romaji: wanakana.toRomaji(row.kana)
+      })
+      if (word.id) words.push(word)
+    }
+    words = words.sort((a, b) => b.head && a.head ? b.head.length - a.head.length : 0)
+    return words
+  },
+  // https://stackoverflow.com/questions/38613654/javascript-find-unique-objects-in-array-based-on-multiple-properties
+  uniqueByValues(arr, keyProps) {
+    const kvArray = arr.map(entry => {
+      const key = keyProps.map(k => entry[k]).join("|");
+      return [key, entry];
+    });
+    const map = new Map(kvArray);
+    return Array.from(map.values());
+  },
+  async loadSmart(name, file) {
+    let data = await localforage.getItem(name)
+    if (!data) {
+      console.log(`EDICT: requesting '${file}' . . .`)
+      let response = await axios.get(file)
+      data = response.data
+      localforage.setItem(name, data)
+      response = null
+    } else {
+      console.log(`EDICT: dictionary '${name}' loaded from local indexedDB via localforage`)
+    }
+    if (data) {
+      return data
+    }
+  },
+  getSize() {
+    return this.words.length
+  },
+  getWords() {
+    return this.words
+  },
+  getWordsThatContain(text) {
+    let strings = []
+    let words = this.words.filter(w => {
+      if (w.kanji && w.kanji.includes(text)) {
+        strings.push(w.kanji)
+      }
+      if (w.kana && w.kana.includes(text)) {
+        strings.push(w.kana)
+      }
+    })
+    return strings
+  },
+  wordForms(word) {
+    let forms = [{
+      table: 'head',
+      field: 'head',
+      form: word.bare
+    }]
+    let jpForms = JPConjugations.conjugate(word.bare)
+    if (jpForms.length === 0) {
+      let tokenized = this.tokenizer.tokenize(word.bare);
+      if (tokenized.length > 0) {
+        jpForms = JPConjugations.conjugate(tokenized[0].basic_form)
+      }
+    }
+
+    forms = forms.concat(jpForms.map(f => {
+      return {
+        table: 'conjugation',
+        field: f.name,
+        form: f.form
+      }
+    }))
+    return forms
+  },
+  stylize(name) {
+    return name
+  },
+  accent(text) {
+    return text
+  },
+  lookupByDef(text, limit = 30) {
+    text = text.toLowerCase()
+    let results = []
+    for (let word of this.words) {
+      for (let d of word.definitions) {
+        let found = d.toLowerCase().includes(text)
+        if (found) {
+          results.push(Object.assign({ score: 1 / (d.length - text.length + 1) }, word))
+        }
+      }
+    }
+    results = results.sort((a, b) => b.score - a.score)
+    return results.slice(0, limit)
+  },
+  lookup(text) {
+    let word = this.words.find(word => word && word.head === text)
+    return word
+  },
+  lookupMultiple(text) {
+    let words = this.words.filter(word => word && (word.kanji === text || word.kana === text))
+    return words
+  },
+  unique(array) {
+    var uniqueArray = []
+    for (let i in array) {
+      if (!uniqueArray.includes(array[i])) uniqueArray.push(array[i])
+    }
+    return uniqueArray
+  },
+  /**
+   * Get a word by ID.
+   * @param {*} id the word's id
+   * @param {*} head (optional) the head of the word to check if matches the word retrieved; if mismatched, we'll look for a matching word instead.
+   * @returns 
+   */
+  get(id, head) {
+    let entry = this.words.find(row => row.id === id)
+    if (head) {
+      if (!entry || entry.head !== head) {
+        entry = this.lookup(head)
+      }
+    }
+    return entry
+  },
+  isChinese(text) {
+    if (this.matchChinese(text)) return true
+  },
+  isRoman(text) {
+    return text.match(/\w+/) ? true : false
+  },
+  matchChinese(text) {
+    return text.match(
+      // eslint-disable-next-line no-irregular-whitespace
+      /[\u2E80-\u2E99\u2E9B-\u2EF3\u2F00-\u2FD5\u3005\u3007\u3021-\u3029\u3038-\u303B‌​\u3400-\u4DB5\u4E00-\u9FCC\uF900-\uFA6D\uFA70-\uFAD9]+/g
+    )
+  },
+  randomArrayItem(array, start = 0, length = false) {
+    length = length || array.length
+    array = array.slice(start, length)
+    let index = Math.floor(Math.random() * array.length)
+    return array[index]
+  },
+  random() {
+    return this.randomArrayItem(this.words)
+  },
+  lookupByCharacter(char) {
+    return this.words.filter(row => row.kanji && row.kanji.includes(char))
+  },
+  lookupKana(kana) {
+    const candidates = this.words.filter(row => {
+      return row.kana === kana
+    })
+    return candidates
+  },
+  lookupByPattern(pattern) {
+    // pattern like '～体'
+    var results = []
+    if (pattern.includes('～')) {
+      const regexPattern = '^' + pattern.replace(/～/gi, '.+') + '$'
+      const regex = new RegExp(regexPattern)
+      results = this.words.filter(word => regex.test(word.kana))
+    } else {
+      results = this.words.filter(word => word.kana.includes(pattern))
+    }
+    return results
+  },
+  lookupFuzzy(text, limit = false) {
+    let results = []
+    if (!this.isRoman(text)) {
+      try {
+        let reg = new RegExp(text, 'gi')
+        results = this.words
+          .filter(
+            row => reg.test(row.kanji) || reg.test(row.kana)
+          )
+      } catch (err) {
+        console.log(err)
+      }
+    } else {
+      text = text.toLowerCase().trim()
+      results = this.words
+        .filter(row =>
+          row.romaji && row.romaji.includes(
+            text.replace(/ /g, '')
+          )
+        )
+    }
+    if (results) {
+      results = results
+        .sort((a, b) => {
+          return a.kana && b.kana ? a.kana.length - b.kana.length : 0
+        })
+      if (limit) {
+        results = results.slice(0, limit)
+      }
+      let shortest = Math.min(...results.map(r => r.kana ? r.kana.length : r.head.length))
+      results = results.map(word => {
+        let w = word.kana || word.head
+        let score = shortest / w.length - 0.1 * Math.min(w.replace(text, '').length, word.kanji.replace(text, '').length)
+        return Object.assign({ score }, word)
+      })
+      return results
+    }
+  },
+  subdict(data) {
+    let newDict = Object.assign({}, this)
+    return Object.assign(newDict, { words: data })
+  },
+  subdictFromText(text) {
+    return this.subdict(
+      this.words.filter(function (row) {
+        return text.includes(row.head)
+      })
+    )
+  },
+  /* Returns the longest word in the dictionary that is inside `text` */
+  longest(text) {
+    // Only return the *first* seen word and those the same as it
+    let first = false
+    let matches = this.words
+      .filter(function (word) {
+        if (first) {
+          return word.head === first
+        } else {
+          if (text.includes(word.head)) {
+            first = word.head
+            return true
+          }
+        }
+      })
+      .sort((a, b) => {
+        return b.head.length - a.head.length
+      })
+    return {
+      matches: matches,
+      text: matches && matches.length > 0 ? matches[0].head : ''
+    }
+  },
+  tokenize(text) {
+    if (this.tokenizationCache[text]) return this.tokenizationCache[text]
+    let t = []
+    let segs = text.split(/\s+/)
+    for (let seg of segs) {
+      let tokenized = this.tokenizer.tokenize(seg);
+      for (let index in tokenized) {
+        let token = tokenized[index]
+        let candidates = this.lookupMultiple(
+          token.surface_form
+        );
+        if (token.basic_form && token.basic_form !== token.surface_form) {
+          candidates = candidates.concat(
+            this.lookupMultiple(
+              token.basic_form
+            )
+          );
+        }
+        t.push({
+          text: token.surface_form,
+          candidates,
+          pos: token.pos
+        })
+      }
+      t.push(' ')
+    }
+    t.pop()
+    return t
+  },
+}
