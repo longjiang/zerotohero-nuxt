254889032867149c27066c1d83c1161a29506b1f
Jiang Long <longjiang2005@gmail.com>
2023-03-15 16:04:18 -0700

    Show Russian accents in word lists.

--- components/WordList.vue
+++ components/WordList.vue
@@ -44,7 +44,7 @@
             <span v-if="$l2.code === 'de' && word.gender">
               {{ { n: "das", m: "der", f: "die" }[word.gender] }}
             </span>
-            {{ word.head }}
+            {{ word.accented || word.head }}
           </span>
 
           <span :class="{ transparent: hidePhonetics }">
