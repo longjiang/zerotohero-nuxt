ceddda440b218d2a0e37bcb085281e45cef425f2
Jiang Long <longjiang2005@gmail.com>
2023-03-19 14:51:04 -0700

    Localize the "you need pro" message.

--- components/YouNeedPro.vue
+++ components/YouNeedPro.vue
@@ -8,7 +8,7 @@
           <template v-if="message">{{ message }}</template>
           <template
             v-else
-          >See complete subtitles with Pro.</template>
+          >{{ $t('See complete subtitles with Pro.') }}</template>
         </p>
       </div>
       <div class="mt-3" />
@@ -24,44 +24,23 @@
             <b style="position: relative; bottom: 0.9rem">.50</b>
           </span>
           <sup style="font-size: 1rem" class="text-success">
-            <b>/ lifetime</b><br/>
+            <b>/ {{ $t('lifetime') }}</b><br/>
           </sup>
         </span>
         <span v-else>
           <sup style="font-size: 1rem">$</sup>
           <span style="font-size: 2.2rem; font-weight: bold">89</span>
           <sup style="font-size: 1rem" class="text-success">
-            <b>/ lifetime!</b>
+            <b>/ {{ $t('lifetime') }}</b>
           </sup>
         </span>
       </div>
       <div class="mt-1" />
-      <div class="strong text-white mb-2" style="font-size: 0.85em">Pay once, enjoy forever!</div>
+      <div class="strong text-white mb-2" style="font-size: 0.85em">{{ $t('Pay once, enjoy forever!') }}</div>
       <div class="mt-4" />
       <div>
-        <router-link class="btn btn-success pl-3 pr-3" :to="{ name: 'go-pro' }">🚀 Upgrade to Pro</router-link>
+        <router-link class="btn btn-success pl-3 pr-3" :to="{ name: 'go-pro' }">🚀 {{ $t('Upgrade to Pro') }}</router-link>
       </div>
-      <!-- <div style="font-size: 0.8em; opacity: 0.7" class="mt-3">
-        Compare prices with:
-        <a
-          href="https://www.fluentu.com/en/pricing/"
-          target="_blank"
-          class="text-primary"
-        >FluentU</a>
-        ,
-        <a
-          href="https://spanish.yabla.com/subscribe.php"
-          target="_blank"
-          class="text-primary"
-        >Yabla</a>
-        , and
-        <a
-          href="https://www.lingq.com/en/signup/"
-          target="_blank"
-          class="text-primary"
-        >LingQ</a>
-        .
-      </div> -->
     </div>
   </div>
 </template>
@@ -89,7 +68,6 @@ export default {
       if (typeof this.$store.state.settings.l2 !== "undefined")
         return this.$store.state.settings.l2;
     },
-
     $adminMode() {
       if (typeof this.$store.state.settings.adminMode !== "undefined")
         return this.$store.state.settings.adminMode;
