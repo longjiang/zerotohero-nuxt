e25296aeaa22b3f2f9653995a62be42eda408680
Jiang Long <longjiang2005@gmail.com>
2023-03-26 17:28:23 -0700

    WordBlockQuiz looks too small

--- components/WordBlockQuiz.vue
+++ components/WordBlockQuiz.vue
@@ -1,45 +1,17 @@
 <template>
-  <div class="word-block-quiz">
+  <div class="word-block-quiz word-block-segment">
     <span class="transparent">{{ text }}</span>
   </div>
 </template>
 
 <script>
 export default {
-
   props: {
-    popup: {
-      default: true,
-    },
-    sticky: {
-      default: false, // whether or not to show each word's level color by default (without hovering)
-    },
-    common: {
-      default: false,
-    },
-    seen: {
-      default: false, // whether this word has already been annotated ('seen') before
-    },
-    saved: {
-      default: false,
-    },
-    obscure: {
-      default: false,
-    },
-    definition: {
-      default: undefined,
-    },
     text: {
-      default: ""
+      default: "",
     },
-    hanja: {
-      default: undefined
-    },
-    phonetics: {
-      default: undefined
-    }
   },
-}
+};
 </script>
 
 <style scoped>
