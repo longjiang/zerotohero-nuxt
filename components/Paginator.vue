13ee9cee8d7ce6f05ce0b18732ea63a2ce560307
Jiang Long <longjiang2005@gmail.com>
2023-04-19 15:18:37 -0700

    Add keyboard shortcuts to paginator.

--- components/Paginator.vue
+++ components/Paginator.vue
@@ -1,47 +1,24 @@
 <template>
   <div class="paginator">
-    <router-link
-      :class="`paginator-previous mr-2 btn btn-xs btn-${$skin} mb-1`"
-      :to="{ path: previousPath, query: $route.query }"
-      title="Previous word"
-      v-if="previous"
-    >
+    <router-link :class="`paginator-previous mr-2 btn btn-xs btn-${$skin} mb-1`" title="Previous (P)"
+      :to="{ path: previousPath, query: $route.query }" :title="$t('Previous') + ' (P)'" v-if="previous">
       <i class="fas fa-chevron-left"></i>
     </router-link>
     <b>
-      <component
-        :is="home ? 'router-link' : 'span'"
-        class="link-unstyled"
-        v-if="title"
-        :to="home"
-      >
+      <component :is="home ? 'router-link' : 'span'" class="link-unstyled" v-if="title" :to="home">
         {{ title }}
       </component>
     </b>
-    <component
-      :is="home ? 'router-link' : 'span'"
-      class="link-unstyled"
-      v-if="home"
-      :to="home"
-    >
+    <component :is="home ? 'router-link' : 'span'" class="link-unstyled" v-if="home" :to="home">
       {{ currentIndex + 1 }} / {{ items.length }}
     </component>
     <b>
-      <component
-        :is="home ? 'router-link' : 'span'"
-        class="link-unstyled"
-        v-if="append"
-        :to="home"
-      >
+      <component :is="home ? 'router-link' : 'span'" class="link-unstyled" v-if="append" :to="home">
         {{ append }}
       </component>
     </b>
-    <router-link
-      :class="`paginator-previous ml-2 btn btn-xs btn-${$skin} mb-1`"
-      :to="{ path: nextPath, query: $route.query }"
-      title="Next word"
-      v-if="next"
-    >
+    <router-link :class="`paginator-previous ml-2 btn btn-xs btn-${$skin} mb-1`"
+      :to="{ path: nextPath, query: $route.query }" :title="$t('Next') + ' (N)'" v-if="next">
       <i class="fas fa-chevron-right"></i>
     </router-link>
   </div>
@@ -94,7 +71,32 @@ export default {
       return this.url(this.next, this.currentIndex + 1);
     },
   },
-  mounted() {},
+  mounted() {
+    this.bindKeys();
+  },
+  beforeDestroy() {
+    this.unbindKeys();
+  },
+  methods: {
+    bindKeys() {
+      window.addEventListener("keydown", this.handleKeydown);
+    },
+    unbindKeys() {
+      window.removeEventListener("keydown", this.handleKeydown);
+    },
+    handleKeydown(e) {
+      if (e.code == "KeyN") {
+        this.$router.push({ path: this.previousPath, query: this.$route.query });
+        e.preventDefault();
+        return false;
+      }
+      if (e.code == "KeyP") {
+        this.$router.push({ path: this.nextPath, query: this.$route.query });
+        e.preventDefault();
+        return false;
+      }
+    }
+  }
 };
 </script>
 
