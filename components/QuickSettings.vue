c0f9fe729d0857301204c944bb1cb936f0107151
Jiang Long <longjiang2005@gmail.com>
2023-04-19 12:01:15 -0700

    Improved quick settings.

--- components/QuickSettings.vue
+++ components/QuickSettings.vue
@@ -1,9 +1,9 @@
 <template>
   <div :class="`annotation-settings annotation-settings-${variant}`">
-    <div class="bg-gray rounded" :class='{
+    <div class="bg-gray rounded p-2" :class='{
       "show-pinyin": $l2Settings.showPinyin,
       "show-pinyin-for-saved":
-        !$l2Settings.showPinyin && l2 && l2.han,
+        !$l2Settings.showPinyin && $l2 && $l2.han,
       "show-simplified": !$l2Settings.useTraditional,
       "show-traditional": $l2Settings.useTraditional,
       "show-definition": $l2Settings.showDefinition,
@@ -13,18 +13,17 @@
       "use-serif": $l2Settings.useSerif,
       [`zerotohero-zoom-${$l2Settings.zoomLevel}`]: true,
     }'>
-      <Annotate class="text-center" :useZoom="true"><span>本は</span></Annotate>
+      <Annotate class="text-center" :useZoom="true"><span>例は</span></Annotate>
+      <div class="translation-line text-center">{{ $t('Example Translation') }}</div>
+    </div>
+    <div class="text-size">
+      <div class="text-size-label">
+        <div style="font-size: 0.8rem">{{ $t('Smaller') }}</div>
+        <div style="font-size: 1.2rem">{{ $t('Bigger') }}</div>
+      </div>
+      <b-form-input type="range" min="0" max="7" step="1" v-model="zoomLevel"></b-form-input>
     </div>
     <div class="quick-settings-language-specific" v-if="$l1 && $l2">
-      <Toggle v-model="quizMode" label="Quiz Mode">
-        <i class="far fa-rocket-launch"></i>
-      </Toggle>
-      <Toggle v-model="autoPronounce" label="Auto Pronounce">
-        <i class="fa fa-volume-up"></i>
-      </Toggle>
-      <Toggle v-model="showDefinition" label="Show Definition">
-        <i class="fa-solid fa-circle-info"></i>
-      </Toggle>
       <Toggle v-model="showPinyin" label="Show Phonetics">
         <span style="font-size: 0.8em; font-weight: bold">
           <ruby v-if="$l2.han" style="position: relative; bottom: -0.1rem">
@@ -44,16 +43,19 @@
           <span v-else>[pʰ]</span>
         </span>
       </Toggle>
+      <Toggle v-model="showDefinition" label="Show Definition">
+        <i class="fa-solid fa-circle-info"></i>
+      </Toggle>
       <Toggle v-if="$l2.han" v-model="useTraditional" :label="useTraditional ? 'Traditional' : 'Simplified'">
         <span>
           <span v-if="useTraditional">繁</span>
           <span v-if="!useTraditional">简</span>
         </span>
       </Toggle>
-      <Toggle v-model="showTranslation" label="Translation">
+      <Toggle v-model="showTranslation" label="Show Translation">
         <i class="fas fa-language"></i>
       </Toggle>
-      <Toggle v-model="showQuickGloss" label="Show Quick Gloss">
+      <Toggle v-model="showQuickGloss" label="Show Gloss for Saved">
         <i class="fas fa-text-size"></i>
       </Toggle>
       <Toggle v-if="['ko', 'vi'].includes($l2.code)" v-model="showByeonggi"
@@ -63,22 +65,17 @@
           <small style="font-size: 0.5em">字</small>
         </span>
       </Toggle>
-
-      <button class="btn btn-unstyled d-block p-0 annotation-setting-toggle"
-        @click="zoomLevel = zoomLevel ? Math.max(zoomLevel - 1, 0) : 0">
-        <span class="annotation-setting-icon">ᴛ</span>
-        {{ $tb("Smaller text") }}
-      </button>
-      <button class="btn btn-unstyled d-block p-0 annotation-setting-toggle"
-        @click="zoomLevel = zoomLevel ? Math.min(zoomLevel + 1, 7) : 1">
-        <span class="annotation-setting-icon">T</span>
-        {{ $tb("Bigger text") }}
-      </button>
       <hr />
     </div>
     <div class="quick-settings-general">
+      <Toggle v-model="autoPronounce" label="Auto Pronounce">
+        <i class="fa fa-volume-up"></i>
+      </Toggle>
       <Toggle v-model="darkMode" label="Dark Mode">
         <i class="fa fa-moon"></i>
+      </Toggle>
+      <Toggle v-model="quizMode" label="Quiz Mode">
+        <i class="far fa-rocket-launch"></i>
       </Toggle>
       <Toggle v-if="userIsAdmin" v-model="adminMode" label="Admin Mode">
         <i class="fa fa-wrench"></i>
@@ -205,6 +202,23 @@ export default {
 </script>
 <style lang="scss">
 @import "~@/assets/scss/variables.scss";
+
+.translation-line {
+  font-size: 0.8em;
+  color: #444;
+  display: none;
+}
+
+.show-translation .translation-line {
+  display: inherit;
+}
+
+.text-size-label {
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  margin: 0.25rem 0 0 0;
+}
 
 .toggle-wrapper {
   margin: 0.25rem 0;
