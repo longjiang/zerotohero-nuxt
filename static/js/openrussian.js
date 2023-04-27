d8929fd3f098e7f48555ab99e9658fa1f649d2d0
Jiang Long <longjiang2005@gmail.com>
2023-04-27 16:01:11 -0700

    Make dictionary into a class

--- static/js/openrussian.js
+++ static/js/openrussian.js
@@ -3,21 +3,33 @@ importScripts('../js/dictionary-utils.js')
 importScripts('../js/tokenizers/russian-tokenizer.js')
 importScripts("../js/inflectors/inflector-factory.js")
 
-const Dictionary = {
-  words: [],
-  l1: undefined,
-  l2: undefined,
+class Dictionary {
+  constructor({ l1 = undefined, l2 = undefined } = {}) {
+    this.words = [];
+    this.l1 = l1;
+    this.l2 = l2;
+    this.loadWords();
+    this.tokenizer = new RussianTokenizer();
+    this.inflector = InflectorFactory.createInflector(this.l2);
+  }
+
   credit() {
-    return 'The Russian dictionary is provided by <a href="https://en.openrussian.org/about">OpenRussian.org</a>, which is freely distribtued.'
-  },
-  async load({ l1 = undefined, l2 = undefined } = {}) {
-    this.l1 = l1
-    this.l2 = l2
-    await this.loadWords()
-    this.tokenizer = new RussianTokenizer()
-    this.inflector = InflectorFactory.createInflector(this.l2)
-    return this
-  },
+    return 'The Russian dictionary is provided by <a href="https://en.openrussian.org/about">OpenRussian.org</a>, which is freely distribtued.';
+  }
+
+  async loadWords() {
+    console.log('OpenRussian: Loading words');
+    let data = await this.loadSmart('words');
+    for (let row of data) {
+      if (row.accented) {
+        row.accented = this.accent(row.accented);
+      }
+      if (row.bare) row.head = row.bare;
+      delete row.audio;
+      row.pos = row.type;
+      this.words[row.id] = row;
+    }
+  }
   async loadSmart(name) {
     const server = 'https://server.chinesezerotohero.com/'
     let data = await localforage.getItem(name)
@@ -37,23 +49,10 @@ const Dictionary = {
       })
       return results.data
     }
-  },
-  async loadWords() {
-    console.log('OpenRussian: Loading words')
-    let data = await this.loadSmart('words')
-    for (let row of data) {
-      if (row.accented) {
-        row.accented = this.accent(row.accented)
-      }
-      if (row.bare) row.head = row.bare
-      delete row.audio
-      row.pos = row.type
-      this.words[row.id] = row
-    }
-  },
+  }
   async tokenize(text) {
     return await this.tokenizer.tokenizeWithCache(text)
-  },
+  }
   /**
    * Get a word by ID.
    * @param {*} id the word's id
@@ -67,50 +66,50 @@ const Dictionary = {
       word = this.lookup(head)
     }
     return word
-  },
+  }
   lookupByDef(text, limit = 30) {
     text = text.toLowerCase()
     let words = this.words
       .filter(word => word.definitions && word.definitions.join(', ').toLowerCase().includes(text))
       .slice(0, limit)
     return words
-  },
+  }
   lookup(text) {
     let word = this.words.find(word => word && word.bare === text)
     return word
-  },
+  }
   unique(a) {
     return a.filter((item, i, ar) => ar.indexOf(item) === i);
-  },
+  }
   getWords() {
     return this.words
-  },
+  }
   getWordsThatContain(text) {
     let words = this.words.filter(w => (w.head.includes(text)) || (w.bare.includes(text)))
     let strings = words
       .map((word) => word.bare)
       .concat(words.map((word) => word.head))
     return this.unique(strings)
-  },
+  }
   lookupMultiple(text) {
     let words = this.words.filter(word => word && word.bare.toLowerCase() === text.toLowerCase())
     return words
-  },
+  }
 
   async inflect(text) {
     return await this.inflector.inflectWithCache(text)
-  },
+  }
 
   lookupFuzzy(text) {
     return this.lookupMultiple(text)
-  },
+  }
   // Called from the dictionary page
   random() {
     return randomProperty(this.words)
-  },
+  }
   accent(text) {
     return text.replace(/'/g, '́')
-  },
+  }
   getSize() {
     return this.words.length
   }
--- static/workers/dict-worker.js
+++ static/workers/dict-worker.js
@@ -2,29 +2,29 @@ importScripts('../vendor/papaparse/papaparse.min.js')
 importScripts('../vendor/axios/axios.min.js')
 importScripts("../vendor/localforage/localforage.js")
 
-let ready = false
+let ready = false;
+let dictionaryInstance;
 
-onmessage = async function(e) {
-  const id = e.data[0]
-  const method = e.data[1]
-  const args = e.data[2]
-  if (method === 'load') {
-    let moduleName = args[0]
-    let options = args[1]
-    importScripts(`../js/${moduleName}.js?v=2.2.4.2`)
-    Dictionary.load(options).then(() => {
-      ready = true
-      this.postMessage([1, 'load', 'ready'])
-    })
-  } else if (method === 'dictionaryMethods') {
-    this.postMessage([id, 'dictionaryMethods', Object.keys(Dictionary)])
+onmessage = async function (e) {
+  const id = e.data[0];
+  const method = e.data[1];
+  const args = e.data[2];
+  if (method === "load") {
+    let moduleName = args[0];
+    let options = args[1];
+    importScripts(`../js/${moduleName}.js?v=2.2.4.2`);
+
+    dictionaryInstance = new Dictionary(options);
+    ready = true;
+    this.postMessage([1, "load", "ready"]);
+  } else if (method === "dictionaryMethods") {
+    this.postMessage([id, "dictionaryMethods", Object.getOwnPropertyNames(Dictionary.prototype)]);
   } else {
-    if (typeof Dictionary[method] !== 'undefined') {
-      let data = await Dictionary[method](...args)
-      this.postMessage([id, method, data])
+    if (typeof dictionaryInstance[method] !== "undefined") {
+      let data = await dictionaryInstance[method](...args);
+      this.postMessage([id, method, data]);
     } else {
-      this.postMessage([id, method, undefined])
+      this.postMessage([id, method, undefined]);
     }
   }
-}
-
+};
