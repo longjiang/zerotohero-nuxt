5a7558d3cb48b3048aabe8b668d7ec73a6ac6bcd
Jiang Long <longjiang2005@gmail.com>
2023-05-18 16:52:22 -0700

    Open Russian: load wiktionary words to get better IPA transcriptions.

--- static/js/open-russian-dictionary.js
+++ static/js/open-russian-dictionary.js
@@ -6,6 +6,7 @@ class OpenRussianDictionary extends BaseDictionary {
   constructor({ l1 = undefined, l2 = undefined } = {}) {
     super({l1, l2});
     this.file = `${SERVER}data/openrussian/words_with_definitions_pronunciation.csv.txt`
+    this.wiktionaryFile = "https://server.chinesezerotohero.com/data/wiktionary-csv/rus-eng.csv.txt"
     this.indexDbVerByLang = {
       rus: '2.18.7',
     };
@@ -17,9 +18,12 @@ class OpenRussianDictionary extends BaseDictionary {
 
   async loadData() {
     let words = await this.loadAndNormalizeDictionaryData({ name: 'open-russian', file: this.file })
+    let wiktionaryWords = await this.loadDictionaryData({ name: `wiktionary-rus-eng`, file: this.wiktionaryFile })
     words = words.filter(word => word && word.head)
-    this.words = words
+    wiktionaryWords.forEach(this.normalizeWiktionaryWord)
+    this.words = [...words, ...wiktionaryWords];
     words = null
+    wiktionaryWords = null
   }
 
   // Normalizes the input 'row' object and modifies it in place.
@@ -32,6 +36,19 @@ class OpenRussianDictionary extends BaseDictionary {
       row.search = row.head.toLowerCase()
       row.definitions = row.definitions.split('|')
     }
+  }
+
+  normalizeWiktionaryWord(item) {
+    const head = item.word.replace(/^\-/, '')
+    item.head = head;
+    item.bare = head;
+    item.search = head.toLowerCase();
+    item.definitions = item.definitions ? item.definitions.split("|") : [];
+    item.id = "w" + hash(item.head + item.definitions[0]);
+    delete item.han;
+    delete item.word;
+    delete item.stems
+    delete item.phrases
   }
   
   lookup(text) {
