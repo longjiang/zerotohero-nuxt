31bd2d5477398af48dc8c8d80038ccdd4d839dc9
Jiang Long <longjiang2005@gmail.com>
2023-09-19 22:30:22 -0700

    Do not use simplemma for Hindi

--- static/js/tokenizers/tokenizer-factory.js
+++ static/js/tokenizers/tokenizer-factory.js
@@ -10,7 +10,8 @@ const TokenizerFactory = {
     EnglishTokenizer: ["eng"], // locally tokenized by splitting, locally lemmatized by javascript-lemmatizer
     RussianTokenizer: ["rus"], // remotely tokenized and lemmatized by pymorphy2
     // We remove 'cym' from SimplemmaTokenizer because it does not treat apostrophes correctly
-    SimplemmaTokenizer: ['ast', 'bul', 'cat', 'ces', 'dan', 'deu', 'ell', 'eng', 'enm', 'est', 'fin', 'fra', 'fra', 'gla', 'gle', 'glg', 'glv', 'hbs', 'hin', 'hun', 'hye', 'ind', 'isl', 'ita', 'kat', 'lat', 'lav', 'lit', 'ltz', 'mkd', 'msa', 'nld', 'nno', 'nob', 'pol', 'por', 'por', 'ron', 'rus', 'slk', 'slv', 'sme', 'spa', 'sqi', 'swa', 'swe', 'tgl', 'tur', 'ukr'], // tokenized and lemmatized by simplemma-tokenizer.js
+    // We remove 'hin' from SimplemmaTokenizer because it breaks too many words
+    SimplemmaTokenizer: ['ast', 'bul', 'cat', 'ces', 'dan', 'deu', 'ell', 'eng', 'enm', 'est', 'fin', 'fra', 'fra', 'gla', 'gle', 'glg', 'glv', 'hbs', 'hun', 'hye', 'ind', 'isl', 'ita', 'kat', 'lat', 'lav', 'lit', 'ltz', 'mkd', 'msa', 'nld', 'nno', 'nob', 'pol', 'por', 'por', 'ron', 'rus', 'slk', 'slv', 'sme', 'spa', 'sqi', 'swa', 'swe', 'tgl', 'tur', 'ukr'], // tokenized and lemmatized by simplemma-tokenizer.js
     LemmatizationListTokenizer: ["ast", "bul", "cat", "ces", "cym", "deu", "eng", "est", "fas", "fra", "gla", "gle", "glg", "glv", "hun", "ita", "por", "ron", "rus", "slk", "slv", "spa", "swe", "ukr"], // tokenized and lemmatized by lemmatization list
     SpacyTokenizer: [
       // "hrv", // tokenized and lemmatized by spacy // too slow
