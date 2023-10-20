d00555c82103046d740e4e8428df6ae60cb109da
Jiang Long <longjiang2005@gmail.com>
2023-10-20 12:09:05 -0700

    Add safe padding to toast.

--- components/YouTubeViewComp.vue
+++ components/YouTubeViewComp.vue
@@ -239,7 +239,7 @@ export default {
           this.$tb("This is a {level} level video.", { level: levelName }),
           {
             position: "top-center",
-            className: `toasted toasted-primary default bg-level${l}`,
+            className: `toasted toasted-primary default bg-level${l} safe-toast safe-padding-top`,
             iconPack: "custom-class",
             icon: "fa-solid fa-signal-bars mr-1",
             duration: 5000,
