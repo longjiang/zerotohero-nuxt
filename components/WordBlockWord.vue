d20af88342264239739c9643f7c9995ebf151391
Jiang Long <longjiang2005@gmail.com>
2023-10-22 15:51:36 -0700

    Do not scope wordblock css to use less dom.

--- components/WordBlockWord.vue
+++ components/WordBlockWord.vue
@@ -77,7 +77,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "@/assets/scss/variables.scss";
 // [data-hover-level=outside].saved {
 //   color: white !important;
