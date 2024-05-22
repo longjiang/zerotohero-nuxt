abe241040229b28b235144893388a096ac4f7fae
Jiang Long <longjiang2005@gmail.com>
2024-05-22 03:00:16 -0700

    Remove 'ads_conversion_Sign_Up_1' tag

--- pages/verify-email.vue
+++ pages/verify-email.vue
@@ -94,7 +94,6 @@ export default {
         if (res.data.status = 'success') {
           this.$toast.success(this.$tb('Email verified. You can now log in.'), { duration: 5000 });
           this.$gtag.event('user_register')
-          this.$gtag.event('ads_conversion_Sign_Up_1')
           // Log out the user
           await this.$auth.logout();          
           this.$router.push({ name: 'login' });
