b159e88368b62ec247eed6f7736d74e093ecacbe
Jiang Long <longjiang2005@gmail.com>
2024-05-23 14:26:33 -0700

    Dynamically load prices.

--- components/PurchaseStripe.vue
+++ components/PurchaseStripe.vue
@@ -53,6 +53,26 @@ export default {
       default: 'lifetime'
     },
   },
+  data() {
+    return {
+      TEST,
+      type: SALE && this.plan === 'lifetime' ? 'sale' : 'regular', // or 'sale'
+      stripePublishableKey: TEST ? "pk_test_kjsYSddsuYbdHnea22KggNe4" : "pk_live_9lnc7wrGHtcFdPKIWZdy9p17",
+      stripeCancelURL: HOST + "/go-pro",
+      productID: TEST ? "prod_NgFwoKIzOmLCA3" : "prod_Lri8TEDq5YDfjb",
+      prices: [],
+    };
+  },
+  async created() {
+    const testQuery = this.TEST ? '?test=true' : '';
+    const response = await fetch(`${PYTHON_SERVER}/stripe-prices${testQuery}`);
+    if (response.ok) {
+      const data = await response.json();
+      this.prices = data;
+    } else {
+      console.error('Failed to load prices:', response.statusText);
+    }
+  },
   computed: {
     usdPrice() {
       let matchedPrices = this.prices.filter(p => p.plan === this.plan && p.type === this.type && p.currency === 'usd')
@@ -75,89 +95,6 @@ export default {
     stripeSuccessURL() {
       return this.$auth.user?.id ? `${PYTHON_SERVER}stripe_checkout_success?user_id=${this.$auth.user.id}&host=${HOST}&session_id={CHECKOUT_SESSION_ID}` : undefined // Make sure we have the user's id
     }
-  },
-  data() {
-    return {
-      TEST,
-      type: SALE && this.plan === 'lifetime' ? 'sale' : 'regular', // or 'sale'
-      stripePublishableKey: TEST ? "pk_test_kjsYSddsuYbdHnea22KggNe4" : "pk_live_9lnc7wrGHtcFdPKIWZdy9p17",
-      stripeCancelURL: HOST + "/go-pro",
-      productID: TEST ? "prod_NgFwoKIzOmLCA3" : "prod_Lri8TEDq5YDfjb",
-      prices: [
-        {
-          plan: 'lifetime',
-          type: 'regular',
-          mode: 'payment',
-          id: TEST ? "price_1PJYA4G5EbMGvOafeWPUPK89" : "price_1PJXzFG5EbMGvOafUQQsWQds",
-          currency: "usd",
-          amount: 129,
-        },
-        {
-          plan: 'lifetime',
-          type: 'regular',
-          mode: 'payment',
-          id: TEST ? "price_1PJYAnG5EbMGvOafc2XNa6Je" : "price_1PJY5tG5EbMGvOafcuY2KHsq",
-          currency: "cny",
-          amount: 908,
-          paymentLink: TEST ? "https://buy.stripe.com/test_7sI4gKgy1adBeqs28c" : "https://buy.stripe.com/7sI9E1gbhdDZg4E6oA"
-        },
-        {
-          plan: 'lifetime',
-          type: 'sale',
-          mode: 'payment',
-          id: TEST ? "price_1PJYS1G5EbMGvOafSaut2Fre" : "price_1PJYNdG5EbMGvOafjNbWU2CV",
-          currency: "usd",
-          amount: 103,
-        },
-        {
-          plan: 'lifetime',
-          type: 'sale',
-          mode: 'payment',
-          id: TEST ? "price_1PJYQZG5EbMGvOafogHhhg3V" : "price_1PJYOLG5EbMGvOafx7KQD30V",
-          currency: "cny",
-          amount: 726,
-          paymentLink: TEST ? "https://buy.stripe.com/test_dR6dRkchLetR8249AF" : "https://buy.stripe.com/4gwg2p3ovfM7cSsaER"
-        },
-        {
-          plan: 'annual',
-          type: 'regular',
-          mode: 'subscription',
-          id: TEST ? "price_1MutQRG5EbMGvOafgfJaciGg" : "price_1MuVSwG5EbMGvOafWG8aC89o",
-          currency: "usd",
-          amount: 59,
-        },
-        {
-          plan: 'annual',
-          type: 'regular',
-          mode: 'payment', // not a re-occurring payment so it can work with Alipay and WeChat Pay
-          id: TEST ? "price_1MutQPG5EbMGvOaf3ekfgqMv" : "price_1MuhpjG5EbMGvOaf5NyI7JMT",
-          currency: "cny",
-          amount: 399,
-          paymentLink: TEST ? "https://buy.stripe.com/test_dR614ygy10D19685kl" : "https://buy.stripe.com/14kaI51gneI37y87sB",
-        },
-        {
-          plan: 'monthly',
-          type: 'regular',
-          mode: 'subscription',
-          id: TEST ? "price_1MutQQG5EbMGvOafqPRQ6fuJ" : "price_1MuVVsG5EbMGvOafrx27nKxM",
-          currency: "usd",
-          amount: 6,
-        },
-        {
-          plan: "monthly",
-          type: "regular",
-          mode: "payment", // not a re-occurring payment so it can work with Alipay and WeChat Pay
-          id: TEST
-            ? "price_1MutQPG5EbMGvOafheA9KTCi"
-            : "price_1Muhw5G5EbMGvOafXWqu0m1l",
-          currency: "cny",
-          amount: 39,
-          paymentLink: TEST
-            ? "https://buy.stripe.com/test_aEUfZsepT0D1aac3ce"
-            : "https://buy.stripe.com/3cs6rPbV10Rdg4E14e",
-        },
-      ],
-    };
   },
   methods: {
     submitStripeUSD() {
