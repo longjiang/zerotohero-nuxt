91bd5db6c4f07c49f54b2bf6fae0347883186230
Jiang Long <longjiang2005@gmail.com>
2024-05-24 18:08:45 -0700

    Fix "Discover TV Shows"

--- pages/youtube-channels.vue
+++ pages/youtube-channels.vue
@@ -9,7 +9,7 @@
 </router>
 <template>
   <div :class="`youtube-browse skin-${$skin}`">
-    <div class="youtube-browse container pb-5">
+    <div class="youtube-browse container pt-5 pb-5">
       <SocialHead
         :title="`Study ${$l2.name} videos with subs | Language Player`"
         :description="`Watch ${$l2.name} videos and study the ${
@@ -34,8 +34,8 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
-import { languageLevels, LANGS_WITH_LEVELS, maxDifficultyByLevel, minDifficultyByLevel } from "@/lib/utils";
+import { mapState } from "vuex";
+
 
 export default {
   props: {
