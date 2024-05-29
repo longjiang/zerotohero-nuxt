26389c84ae0f005358999b26f5a36fb8831e2503
Jiang Long <longjiang2005@gmail.com>
2024-05-28 19:56:05 -0700

    Oops forgot to go back to live server.

--- static/js/dictionary-utils.js
+++ static/js/dictionary-utils.js
@@ -1,8 +1,8 @@
 // dictionary-utils.js
 
 // const PYTHON_SERVER = "https://python.zerotohero.ca/";  // Unavailable after Mar 31 2024
-// const PYTHON_SERVER = "https://pythonvps.zerotohero.ca/";  // New since Mar 17 2024
-const PYTHON_SERVER = "http://127.0.0.1:5001/";
+const PYTHON_SERVER = "https://pythonvps.zerotohero.ca/";  // New since Mar 17 2024
+// const PYTHON_SERVER = "http://127.0.0.1:5001/";
 
 const SERVER = "https://server.chinesezerotohero.com/";
 
