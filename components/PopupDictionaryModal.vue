c4435fd0e3d616717fdbd8058b1f2493046a5d4b
Jiang Long <longjiang2005@gmail.com>
2024-10-20 08:58:50 +0900

    fix(popup-dictionary-modal): bring back 'close' button.

--- components/PopupDictionaryModal.vue
+++ components/PopupDictionaryModal.vue
@@ -1,16 +1,26 @@
 <template>
+  <!-- no-fade is turned on to prevent a "ghosted" modal on iOS that blocks and disables the entire UI -->
   <b-modal
     ref="popup-dictionary-modal"
     size="sm"
     centered
     hide-footer
     no-fade
-    modal-class="safe-padding-top mt-4"
-    :title="$tb(info ? 'Note' : 'Dictionary')"
+    modal-class="safe-padding-top my-5"
     :body-class="`popup-dictionary-modal-wrapper l2-${$l2 ? $l2.code : ''}`"
+    :header-class="`popup-dictionary-modal-header l2-${$l2 ? $l2.code : ''}`"
     @show="$nuxt.$emit('popupOpened')"
     @hide="$nuxt.$emit('popupClosed')"
   >
+    <template #modal-header="{ close }">
+      <!-- Emulate built in modal header close button action -->
+      <div class="d-flex justify-content-between w-100 align-items-center">
+        <h5 class="mb-0">{{ info ? $tb('Note') : text }}</h5>
+        <b-button size="sm" variant="outline-success" @click="close()">
+          <i class="fas fa-times"></i> {{ $tb("Close") }}
+        </b-button>
+      </div>
+    </template>
     <div
       v-if="info"
       class="popover-inner-hover-area"
@@ -21,18 +31,27 @@
       v-else
       class="popover-inner-hover-area"
     >
+      <div
+        v-if="
+          (this.savedWord || this.savedPhrase) && this.quizMode && !this.reveal
+        "
+        class="popover-inner-hover-area"
+      >
+        {{ $t("Tap to show answer.") }}
+      </div>
       <WordBlockPopup
         v-bind="{
-          words,
-          images,
           text,
           token,
+          words,
+          images,
           lookupInProgress,
           loadingImages,
           context,
           phraseObj,
         }"
         ref="popup"
+        @translation="translation = $event"
       />
     </div>
   </b-modal>
@@ -98,3 +117,9 @@ export default {
   },
 }
 </script>
+<style lang="scss">
+.popup-dictionary-modal-header {
+  border-bottom: none;
+  padding-bottom: 0;
+}
+</style>
