1b15c7ffbdd37a02db22c4dd6ea1f84deae883b8
Jiang Long <longjiang2005@gmail.com>
2026-07-08 23:13:28 -0700

    feat(admin): add a 'confirm-delete' page for showing videos marked as 'deleted'

---
+++ pages/_l1/_l2/confirm-deletion.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container pt-5 pb-5">
+    <div class="row">
+      <div class="col-md-12">
+        <h5 class="text-center">The following videos in {{ $l2?.name }} are marked for deletion.</h5>
+        <p class="text-center text-muted">Delete them in the database directly.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <MediaSearchResults :params="mediaSearchParams" class="mt-4" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mediaSearchParams: {
+        l1: this.$l1,
+        l2: this.$l2,
+        start: 0,
+        limit: 100,
+        status: "deleted",
+      },
+    };
+  },
+};
+</script>
